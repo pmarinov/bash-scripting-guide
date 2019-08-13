5e2021c0500f646d71807b696248df7d7eef3e6e
Peter Marinov <efravia@gmail.com>
2019-08-12 20:14:17 -0700

    New page legal.poly.pm
    - Move legal portions from page0
    - Explain that the original work is by Mendel Cooper
    - Add a license section for Public Domain
    - Add a section for Revision History

--- pages/page0.poly.pm
+++ pages/page0.poly.pm
@@ -6,26 +6,6 @@
 ◊define-meta[page-title]{Top}
 ◊book-title{◊this-book-title}
 
-An in-depth exploration of the art of shell scripting
-
-Mendel Cooper
-
-◊; <thegrendel.abs@gmail.com>
-
-10
-10 Mar 2014
-
-Revision History
-
-Revision 6.5	05 Apr 2012	Revised by: mc
-'TUNGSTENBERRY' release
-
-Revision 6.6	27 Nov 2012	Revised by: mc
-'YTTERBIUMBERRY' release
-
-Revision 10	10 Mar 2014	Revised by: mc
-'PUBLICDOMAIN' release
-
 This tutorial assumes no previous knowledge of scripting or
 programming, yet progresses rapidly toward an intermediate/advanced
 level of instruction . . . all the while sneaking in little nuggets of
@@ -38,7 +18,7 @@ way to really learn scripting is to write scripts.
 This book is suitable for classroom use as a general introduction to
 programming concepts.
 
-This document is herewith granted to the Public Domain. No copyright!
+An in-depth exploration of the art of shell scripting.
 
 ◊; List pages under page0.poly.pm
 ◊(node-menu "pages/page0.poly.pm")
