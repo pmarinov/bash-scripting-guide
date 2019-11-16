97ab34c3ff5d2d498b04403ce644376c0bf3340f
Peter Marinov <efravia@gmail.com>
2019-11-16 14:51:06 -0800

    html: Adjustments for page0 (title)
    - Book title is H1 on page0
    - Everywhere else, render page description as H1

--- pages/page0.poly.pm
+++ pages/page0.poly.pm
@@ -7,19 +7,22 @@
 ◊define-meta[page-description]{Page0}
 ◊book-title{◊this-book-title}
 
+Original edition by Mendel Cooper, dedicated to the Public Domain
+
+◊strong{An in-depth exploration of the art of shell scripting.}
+
 This tutorial assumes no previous knowledge of scripting or
 programming, yet progresses rapidly toward an intermediate/advanced
 level of instruction... ◊emphasize{all the while sneaking in little
 nuggets of UNIX® wisdom and lore.} It serves as a textbook, a manual
 for self-study, and as a reference and source of knowledge on shell
 scripting techniques. The exercises and heavily-commented examples
-invite active reader participation, under the premise that ◊strong{the
-only way to really learn scripting is to write scripts.}
+invite active reader participation, under the premise that
+◊emphasize{the only way to really learn scripting is to write
+scripts.}
 
 This book is suitable for classroom use as a general introduction to
 programming concepts.
-
-An in-depth exploration of the art of shell scripting.
 
 ◊; List pages under page0.poly.pm
 ◊(node-menu "pages/page0.poly.pm")
--- template.html.p
+++ template.html.p
@@ -5,19 +5,21 @@
   <meta charset="UTF-8">
   <title>
     ◊(select-from-metas 'page-title metas) |
-    ◊(select-from-metas 'page-description metas) |
+    ◊;(select-from-metas 'page-description metas) |
     ◊(->html this-book-title)
   </title>
   <link rel="stylesheet" type="text/css" href="../style.css"/>
 </head>
 <body>
 
-◊; TODO: But book title if on page0
-
-<h1>◊(select-from-metas 'page-description metas)</h1>
+◊; On page0 the H1 is the book title
+◊; everywhere else, render H1 from the page description
+◊(when (not (or (equal? here 'pages/page0.html) (equal? here 'pages/page0.poly.pm)))
+  {<h1>(select-from-metas 'page-description metas)</h1>})
 
 ◊(->html doc)
 
+◊; TODO: Add page navigation (prev, next, up)
 ◊;The current page is called ◊|here|.
 
 </body>
