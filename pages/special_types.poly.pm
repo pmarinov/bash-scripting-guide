02a95d94214985a98e5b2da3ba7409428d3ebace
Peter Marinov <efravia@gmail.com>
2019-11-28 22:13:35 -0800

    chapter: Special Variable Types (part 5)

--- pages/special_types.poly.pm
+++ pages/special_types.poly.pm
@@ -201,6 +201,41 @@ fi
 
 ◊strong{Example: ◊emphasize{wh, whois} domain name lookup}
 
+◊example{
+#!/bin/bash
+# ex18.sh
+
+# Does a 'whois domain-name' lookup on any of 3 alternate servers:
+#                    ripe.net, cw.net, radb.net
+
+# Place this script -- renamed 'wh' -- in /usr/local/bin
+
+# Requires symbolic links:
+# ln -s /usr/local/bin/wh /usr/local/bin/wh-ripe
+# ln -s /usr/local/bin/wh /usr/local/bin/wh-apnic
+# ln -s /usr/local/bin/wh /usr/local/bin/wh-tucows
+
+E_NOARGS=75
+
+
+if [ -z "$1" ]
+then
+  echo "Usage: `basename $0` [domain-name]"
+  exit $E_NOARGS
+fi
+
+# Check script name and call proper server.
+case `basename $0` in    # Or:    case ${0##*/} in
+    "wh"       ) whois $1@whois.tucows.com;;
+    "wh-ripe"  ) whois $1@whois.ripe.net;;
+    "wh-apnic" ) whois $1@whois.apnic.net;;
+    "wh-cw"    ) whois $1@whois.cw.net;;
+    *          ) echo "Usage: `basename $0` [domain-name]";;
+esac
+
+exit $?
+}
+
 }
 
 }
