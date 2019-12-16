fb7b96f68aca9e8d819dd0bbe6d261f089948650
Peter Marinov <efravia@gmail.com>
2019-12-15 21:14:51 -0800

    chapter: Special Variable Types (part 6)

--- pages/special_types.poly.pm
+++ pages/special_types.poly.pm
@@ -236,6 +236,89 @@ esac
 exit $?
 }
 
+The ◊code{shift} command reassigns the positional parameters, in
+effect shifting them to the left one notch.
+
+◊code{$1 <--- $2, $2 <--- $3, $3 <--- $4, etc.}
+
+The old ◊code{$1} disappears, but ◊code{$0} (the script name) does not
+change. If you use a large number of positional parameters to a
+script, shift lets you access those past 10, although
+$◊escaped{◊"{"}bracket$◊escaped{◊"}"} notation also permits this.
+
+◊strong{Using ◊code{shift}}
+
+◊example{
+#!/bin/bash
+# shft.sh: Using 'shift' to step through all the positional parameters.
+
+#  Name this script something like shft.sh,
+#+ and invoke it with some parameters.
+#+ For example:
+#             sh shft.sh a b c def 83 barndoor
+
+until [ -z "$1" ]  # Until all parameters used up . . .
+do
+  echo -n "$1 "
+  shift
+done
+
+echo               # Extra linefeed.
+
+# But, what happens to the "used-up" parameters?
+echo "$2"
+#  Nothing echoes!
+#  When $2 shifts into $1 (and there is no $3 to shift into $2)
+#+ then $2 remains empty.
+#  So, it is not a parameter *copy*, but a *move*.
+
+exit
+
+#  See also the echo-params.sh script for a "shiftless"
+#+ alternative method of stepping through the positional params.
+}
+
+The ◊code{shift} command can take a numerical parameter indicating how
+many positions to shift.
+
+◊example{
+#!/bin/bash
+# shift-past.sh
+
+shift 3    # Shift 3 positions.
+#  n=3; shift $n
+#  Has the same effect.
+
+echo "$1"
+
+exit 0
+
+# ======================== #
+
+
+$ sh shift-past.sh 1 2 3 4 5
+4
+
+#  However, as Eleni Fragkiadaki, points out,
+#+ attempting a 'shift' past the number of
+#+ positional parameters ($#) returns an exit status of 1,
+#+ and the positional parameters themselves do not change.
+#  This means possibly getting stuck in an endless loop. . . .
+#  For example:
+#      until [ -z "$1" ]
+#      do
+#         echo -n "$1 "
+#         shift 20    #  If less than 20 pos params,
+#      done           #+ then loop never ends!
+#
+# When in doubt, add a sanity check. . . .
+#           shift 20 || break
+#                    ^^^^^^^^
+}
+
+Note: The shift command works in a similar fashion on parameters
+passed to a function. TODO: See Example 36-18.
+
 }
 
 }
