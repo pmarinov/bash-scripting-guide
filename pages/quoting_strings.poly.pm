ed0d0ea75fb0077926d44f5d33cc710f6852c8f4
Peter Marinov <efravia@gmail.com>
2019-12-23 21:09:15 -0800

    chapter: Quoting (part 2)

--- pages/quoting_strings.poly.pm
+++ pages/quoting_strings.poly.pm
@@ -3,7 +3,12 @@
 ◊define-meta[page-title]{Quoting strings}
 ◊define-meta[page-description]{Interpretation of strings}
 
-Quoting means just that, bracketing a string in quotes. This has the effect of protecting special characters in the string from reinterpretation or expansion by the shell or shell script. (A character is "special" if it has an interpretation other than its literal meaning. For example, the asterisk * represents a wild card character in globbing and Regular Expressions).
+Quoting means just that, bracketing a string in quotes. This has the
+effect of protecting special characters in the string from
+reinterpretation or expansion by the shell or shell script. (A
+character is "special" if it has an interpretation other than its
+literal meaning. For example, the asterisk * represents a wild card
+character in globbing and Regular Expressions).
 
 ◊example{
 bash$ ls -l [Vv]*
@@ -13,6 +18,38 @@ bash$ ls -l [Vv]*
 
 bash$ ls -l '[Vv]*'
 ls: [Vv]*: No such file or directory
+}
+
+Note: In everyday speech or writing, when we "quote" a phrase, we set
+it apart and give it special meaning. In a Bash ◊emphasize{script},
+when we ◊emphasize{quote} a string, we set it apart and protect its
+◊emphasize{literal} meaning.
+
+Certain programs and utilities reinterpret or expand special
+characters in a quoted string. An important use of quoting is
+protecting a command-line parameter from the shell, but still letting
+the calling program expand it.
+
+◊example{
+bash$ grep '[Ff]irst' *.txt
+file1.txt:This is the first line of file1.txt.
+ file2.txt:This is the First line of file2.txt.
+}
+
+Note that the unquoted ◊code{grep [Ff]irst *.txt} works under the Bash shell.
+◊footnote{Unless there is a file named first in the current working
+directory. Yet another reason to quote. (Thank you, Harald Koenig, for
+pointing this out.)}
+
+◊example{
+bash$ echo $(ls -l)
+total 8 -rw-rw-r-- 1 bo bo 13 Aug 21 12:57 t.sh -rw-rw-r-- 1 bo bo 78 Aug 21 12:57 u.sh
+
+
+bash$ echo "$(ls -l)"
+total 8
+ -rw-rw-r--  1 bo bo  13 Aug 21 12:57 t.sh
+ -rw-rw-r--  1 bo bo  78 Aug 21 12:57 u.sh
 }
 
 ◊; emacs:
