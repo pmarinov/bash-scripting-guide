414e2816ada2c9183a21e4f80a90b6a08b9deebf
Peter Marinov <efravia@gmail.com>
2020-01-30 08:04:59 -0800

    chapter: Exit status (part 2)

--- pages/exit_status.poly.pm
+++ pages/exit_status.poly.pm
@@ -24,6 +24,59 @@ determines the exit status. Within a script, an ◊code{exit nnn}
 command may be used to deliver an ◊code{nnn} exit status to the shell
 (◊code{nnn} must be an integer in the 0 - 255 range).
 
+Note: When a script ends with an ◊code{exit} that has no parameter,
+the exit status of the script is the exit status of the last command
+executed in the script (previous to the ◊code{exit}).
+
+◊example{
+#!/bin/bash
+
+COMMAND_1
+
+. . .
+
+COMMAND_LAST
+
+# Will exit with status of last command.
+
+exit
+}
+
+The equivalent of a bare ◊code{exit} is ◊code{exit $?} or even just
+omitting the exit.
+
+◊example{
+#!/bin/bash
+
+COMMAND_1
+
+. . .
+
+COMMAND_LAST
+
+# Will exit with status of last command.
+
+exit $?
+}
+
+◊example{
+#!/bin/bash
+
+COMMAND1
+
+. . .
+
+COMMAND_LAST
+
+# Will exit with status of last command.
+}
+
+◊code{$?} reads the exit status of the last command executed. After a
+function returns, ◊code{$?} gives the exit status of the last command
+executed in the function. This is Bash's way of giving functions a
+"return value." ◊footnote{In those instances when there is no ◊code{return}
+terminating the function.}
+
 ◊; emacs:
 ◊; Local Variables:
 ◊; mode: fundamental
