d4011332d3cbaa2e0b739a95c3439d25b7055726
Peter Marinov <efravia@gmail.com>
2020-02-05 18:26:34 -0800

    chapter: Test constructs (part 1)

---
+++ pages/test_constructs.poly.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊define-meta[page-title]{Test constructs}
+◊define-meta[page-description]{Tests constructs}
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
---
+++ pages/tests.poly.pm
@@ -0,0 +1,16 @@
+#lang pollen
+
+◊define-meta[page-title]{Tests}
+◊define-meta[page-description]{Tests}
+
+Every reasonably complete programming language can test for a
+condition, then act according to the result of the test. Bash has the
+◊code{test} command, various ◊emphasize{bracket} and
+◊emphasize{parenthesis} operators, and the ◊code{if/then} construct.
+
+◊(node-menu "pages/tests.poly.pm")
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
