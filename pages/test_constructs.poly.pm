0807a0c368407a5b6e44bc7674045256a4db6ff3
Peter Marinov <efravia@gmail.com>
2020-02-13 20:47:54 -0800

    chapter: Test constructs (part 2)

--- pages/test_constructs.poly.pm
+++ pages/test_constructs.poly.pm
@@ -26,6 +26,55 @@ Bash sees ◊code{[[ $a -lt $b ]]} as a single element, which returns an
 exit status.
 }
 
+◊list-entry{The ◊code{(( ... ))} and ◊code{let ...} constructs return
+an exit status, according to whether the arithmetic expressions they
+evaluate expand to a non-zero value. These arithmetic-expansion
+constructs may therefore be used to perform arithmetic comparisons.
+
+◊example{
+(( 0 && 1 ))                 # Logical AND
+echo $?     # 1     ***
+# And so ...
+let "num = (( 0 && 1 ))"
+echo $num   # 0
+# But ...
+let "num = (( 0 && 1 ))"
+echo $?     # 1     ***
+
+
+(( 200 || 11 ))              # Logical OR
+echo $?     # 0     ***
+# ...
+let "num = (( 200 || 11 ))"
+echo $num   # 1
+let "num = (( 200 || 11 ))"
+echo $?     # 0     ***
+
+
+(( 200 | 11 ))               # Bitwise OR
+echo $?                      # 0     ***
+# ...
+let "num = (( 200 | 11 ))"
+echo $num                    # 203
+let "num = (( 200 | 11 ))"
+echo $?                      # 0     ***
+
+# The "let" construct returns the same exit status
+#+ as the double-parentheses arithmetic expansion.
+}
+
+Caution: Again, note that the exit status of an arithmetic expression
+is not an error value.
+
+◊example{
+var=-2 && (( var+=2 ))
+echo $?                   # 1
+
+var=-2 && (( var+=2 )) && echo $var
+                          # Will not echo $var!
+}
+
+}
 
 } ◊; list-block
 
