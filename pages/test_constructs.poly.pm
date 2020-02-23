23431fbb82173b58c1636c921f5c5744674cbae4
Peter Marinov <efravia@gmail.com>
2020-02-22 21:36:40 -0800

    chapter: Test constructs (part 9)

--- pages/test_constructs.poly.pm
+++ pages/test_constructs.poly.pm
@@ -456,6 +456,71 @@ home=/home/bozo
 [ -d "$home" ] || echo "$home directory does not exist."
 }
 
+The ◊code{(( ))} construct expands and evaluates an arithmetic
+expression. If the expression evaluates as zero, it returns an exit
+status of 1, or "false". A non-zero expression returns an exit status
+of 0, or "true". This is in marked contrast to using the ◊code{test}
+and ◊code{[ ]} constructs previously discussed.
+
+◊section-example[#:anchor test_using_parentheses]{Arithmetic Tests
+using (( ))}
+
+◊example{
+#!/bin/bash
+# arith-tests.sh
+# Arithmetic tests.
+
+# The (( ... )) construct evaluates and tests numerical expressions.
+# Exit status opposite from [ ... ] construct!
+
+(( 0 ))
+echo "Exit status of \"(( 0 ))\" is $?."         # 1
+
+(( 1 ))
+echo "Exit status of \"(( 1 ))\" is $?."         # 0
+
+(( 5 > 4 ))                                      # true
+echo "Exit status of \"(( 5 > 4 ))\" is $?."     # 0
+
+(( 5 > 9 ))                                      # false
+echo "Exit status of \"(( 5 > 9 ))\" is $?."     # 1
+
+(( 5 == 5 ))                                     # true
+echo "Exit status of \"(( 5 == 5 ))\" is $?."    # 0
+# (( 5 = 5 ))  gives an error message.
+
+(( 5 - 5 ))                                      # 0
+echo "Exit status of \"(( 5 - 5 ))\" is $?."     # 1
+
+(( 5 / 4 ))                                      # Division o.k.
+echo "Exit status of \"(( 5 / 4 ))\" is $?."     # 0
+
+(( 1 / 2 ))                                      # Division result < 1.
+echo "Exit status of \"(( 1 / 2 ))\" is $?."     # Rounded off to 0.
+                                                 # 1
+
+(( 1 / 0 )) 2>/dev/null                          # Illegal division by 0.
+#           ^^^^^^^^^^^
+echo "Exit status of \"(( 1 / 0 ))\" is $?."     # 1
+
+# What effect does the "2>/dev/null" have?
+# What would happen if it were removed?
+# Try removing it, then rerunning the script.
+
+# ======================================= #
+
+# (( ... )) also useful in an if-then test.
+
+var1=5
+var2=4
+
+if (( var1 > var2 ))
+then #^      ^      Note: Not $var1, $var2. Why?
+  echo "$var1 is greater than $var2"
+fi     # 5 is greater than 4
+
+exit 0
+}
 
 
 ◊; emacs:
