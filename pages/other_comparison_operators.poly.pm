f517861f7688b6bd272cb6333220082882dc74dd
Peter Marinov <efravia@gmail.com>
2020-04-10 22:37:23 -0700

    chapter: Other comparison operators (Part 1)

---
+++ pages/other_comparison_operators.poly.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+◊define-meta[page-title]{Other comparison}
+◊define-meta[page-description]{Other comparison operators}
+
+A binary comparison operator compares two variables or
+quantities. ◊strong{Note that} integer and string comparison use a
+different set of operators.
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
