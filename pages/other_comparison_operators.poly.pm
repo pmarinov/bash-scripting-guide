169e3cc3348aaa1fcae22ea047c44f5bc5d9dddb
Peter Marinov <efravia@gmail.com>
2020-04-16 17:30:03 -0700

    chapter: Other comparison operators (Part 2)

--- pages/other_comparison_operators.poly.pm
+++ pages/other_comparison_operators.poly.pm
@@ -5,7 +5,31 @@
 
 A binary comparison operator compares two variables or
 quantities. ◊strong{Note that} integer and string comparison use a
-different set of operators.
+◊strong{different} set of operators.
+
+◊section{Integer comparison}
+
+◊definition-block[#:type "code"]{
+
+◊definition-entry[#:name "-eq"]{
+Is ◊strong{equal} to
+
+◊example{
+if [ "$a" -eq "$b" ]
+}
+
+}
+
+◊definition-entry[#:name "-ne"]{
+Is ◊strong{no equal} to
+
+◊example{
+if [ "$a" -ne "$b" ]
+}
+
+}
+
+}  ◊; definition-block{}
 
 ◊; emacs:
 ◊; Local Variables:
