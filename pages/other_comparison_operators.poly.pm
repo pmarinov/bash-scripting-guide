8996e720747cad980d24a626757f6eee1bfaf6a7
Peter Marinov <efravia@gmail.com>
2020-04-22 19:35:16 -0700

    chapter: Other comparison operators (Part 4)

--- pages/other_comparison_operators.poly.pm
+++ pages/other_comparison_operators.poly.pm
@@ -106,6 +106,103 @@ Is ◊strong{greater than or equal} to (within double parentheses)
 
 ◊section{String comparison}
 
+◊definition-block[#:type "code"]{
+
+◊definition-entry[#:name "="]{
+Is ◊strong{equal} to
+
+◊example{
+if [ "$a" = "$b" ]
+}
+
+Caution: Note the whitespace framing the ◊code{=}!
+
+◊code{if [ "$a"="$b" ]} is not equivalent to the above.
+
+}
+
+◊definition-entry[#:name "=="]{
+Is ◊strong{equal} to
+
+◊example{
+if [ "$a" == "$b" ]
+}
+
+This is a synonym for ◊code{=}.
+
+Note: The ◊code{==} comparison operator behaves differently within a
+double-brackets test than within single brackets.
+
+◊example{
+[[ $a == z* ]]   # True if $a starts with an "z" (pattern matching).
+[[ $a == "z*" ]] # True if $a is equal to z* (literal matching).
+
+[ $a == z* ]     # File globbing and word splitting take place.
+[ "$a" == "z*" ] # True if $a is equal to z* (literal matching).
+
+# Thanks, Stéphane Chazelas
+}
+
+}
+
+◊definition-entry[#:name "!="]{
+Is ◊strong{not equal} to
+
+◊example{
+if [ "$a" != "$b" ]
+}
+
+This operator uses pattern matching within a ◊code{[[ ... ]]}
+construct.
+
+}
+
+◊definition-entry[#:name "<"]{
+Is ◊strong{less than} in ASCII alphabetical order
+
+◊example{
+if [[ "$a" < "$b" ]]
+
+if [ "$a" \< "$b" ]
+}
+
+Note that the ◊code{<} needs to be escaped within a ◊code{[ ]}
+construct.
+
+}
+
+◊definition-entry[#:name ">"]{
+Is ◊strong{greater than} in ASCII alphabetical order
+
+◊example{
+if [[ "$a" > "$b" ]]
+
+if [ "$a" \> "$b" ]
+}
+
+Note that the ◊code{>} needs to be escaped within a ◊code{[ ]}
+construct.
+
+TODO: See example for an application of this operator
+}
+
+◊definition-entry[#:name "-z"]{
+String ◊strong{is null}, that is, has zero length
+
+◊example{
+String=''   # Zero-length ("null") string variable.
+
+if [ -z "$String" ]
+then
+  echo "\$String is null."
+else
+  echo "\$String is NOT null."
+fi     # $String is null.
+}
+}
+
+}  ◊; definition-block{}
+
 ◊; emacs:
 ◊; Local Variables:
 ◊; mode: fundamental
