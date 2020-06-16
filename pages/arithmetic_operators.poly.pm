6f3a9826516b36cd270aca62e20a9e66a922ea03
Peter Marinov <efravia@gmail.com>
2020-06-15 19:39:33 -0700

    chapter: Arithmetic Operators (part 5)

--- pages/arithmetic_operators.poly.pm
+++ pages/arithmetic_operators.poly.pm
@@ -309,7 +309,218 @@ or math library functions.
 bitwise left shift (multiplies by 2 for each shift position)
 
 }
+
+◊definition-entry[#:name "<<="]{
+left-shift-equal
+
+◊example{
+let "var <<= 2"
 }
+
+Results in var left-shifted 2 bits (multiplied by 4)
+
+}
+
+◊definition-entry[#:name ">>"]{
+bitwise right shift (divides by 2 for each shift position)
+
+}
+
+◊definition-entry[#:name ">>="]{
+right-shift-equal (inverse of ◊code{<<=})
+
+}
+
+◊definition-entry[#:name "&"]{
+right-shift-equal (inverse of ◊code{<<=})
+
+}
+
+◊definition-entry[#:name "&="]{
+bitwise AND-equal
+
+}
+
+◊definition-entry[#:name "|"]{
+bitwise OR
+
+}
+
+◊definition-entry[#:name "|="]{
+bitwise OR-equal
+
+}
+
+◊definition-entry[#:name "~"]{
+bitwise NOT
+
+}
+
+◊definition-entry[#:name "^"]{
+bitwise XOR
+
+}
+
+◊definition-entry[#:name "^="]{
+bitwise XOR-equal
+
+}
+
+} ◊; definition-block{}
+
+◊section{logical (boolean) operators}
+
+◊definition-block[#:type "code"]{
+◊definition-entry[#:name "!"]{
+NOT
+
+◊example{
+if [ ! -f $FILENAME ]
+then
+  ...
+}
+
+}
+
+◊definition-entry[#:name "&&"]{
+AND
+
+◊example{
+if [ $condition1 ] && [ $condition2 ]
+#  Same as:  if [ $condition1 -a $condition2 ]
+#  Returns true if both condition1 and condition2 hold true...
+
+if [[ $condition1 && $condition2 ]]    # Also works.
+#  Note that && operator not permitted inside brackets
+#+ of [ ... ] construct.
+}
+
+Note: ◊code{&&} may also be used, depending on context, in an and list
+to concatenate (TODO: ref) commands.
+
+}
+
+◊definition-entry[#:name "||"]{
+OR
+
+◊example{
+if [ $condition1 ] || [ $condition2 ]
+# Same as:  if [ $condition1 -o $condition2 ]
+# Returns true if either condition1 or condition2 holds true...
+
+if [[ $condition1 || $condition2 ]]    # Also works.
+#  Note that || operator not permitted inside brackets
+#+ of a [ ... ] construct.
+}
+
+Note: Bash tests the exit status of each statement linked with a
+logical operator.
+
+}
+
+} ◊; definition-block{}
+
+◊section-example[#:anchor "compnd_cond1"]{Compound Condition Tests Using
+&& and ||}
+
+◊example{
+#!/bin/bash
+
+a=24
+b=47
+
+if [ "$a" -eq 24 ] && [ "$b" -eq 47 ]
+then
+  echo "Test #1 succeeds."
+else
+  echo "Test #1 fails."
+fi
+
+# ERROR:   if [ "$a" -eq 24 && "$b" -eq 47 ]
+#+         attempts to execute  ' [ "$a" -eq 24 '
+#+         and fails to finding matching ']'.
+#
+#  Note:  if [[ $a -eq 24 && $b -eq 24 ]]  works.
+#  The double-bracket if-test is more flexible
+#+ than the single-bracket version.
+#    (The "&&" has a different meaning in line 17 than in line 6.)
+#    Thanks, Stephane Chazelas, for pointing this out.
+
+
+if [ "$a" -eq 98 ] || [ "$b" -eq 47 ]
+then
+  echo "Test #2 succeeds."
+else
+  echo "Test #2 fails."
+fi
+
+
+#  The -a and -o options provide
+#+ an alternative compound condition test.
+#  Thanks to Patrick Callahan for pointing this out.
+
+
+if [ "$a" -eq 24 -a "$b" -eq 47 ]
+then
+  echo "Test #3 succeeds."
+else
+  echo "Test #3 fails."
+fi
+
+
+if [ "$a" -eq 98 -o "$b" -eq 47 ]
+then
+  echo "Test #4 succeeds."
+else
+  echo "Test #4 fails."
+fi
+
+
+a=rhino
+b=crocodile
+if [ "$a" = rhino ] && [ "$b" = crocodile ]
+then
+  echo "Test #5 succeeds."
+else
+  echo "Test #5 fails."
+fi
+
+exit 0
+}
+
+The ◊code{&&} and ◊code{||} operators also find use in an arithmetic
+context.
+
+◊example{
+bash$ echo $(( 1 && 2 )) $((3 && 0)) $((4 || 0)) $((0 || 0))
+1 0 1 0
+}
+
+◊section{Miscellaneous operators}
+
+◊definition-block[#:type "code"]{
+◊definition-entry[#:name ","]{
+Comma operator
+
+The comma operator chains together two or more arithmetic
+operations. All the operations are evaluated (with possible side
+effects). ◊footnote{Side effects are, of course, unintended -- and
+usually undesirable -- consequences.}
+
+◊example{
+let "t1 = ((5 + 3, 7 - 1, 15 - 4))"
+echo "t1 = $t1"           ^^^^^^  # t1 = 11
+# Here t1 is set to the result of the last operation. Why?
+
+let "t2 = ((a = 9, 15 / 3))"      # Set "a" and calculate "t2".
+echo "t2 = $t2    a = $a"         # t2 = 5    a = 9
+}
+
+The comma operator finds use mainly in for loops. See Example 11-13 (TODO)
+
+}
+
+} ◊; definition-block{}
 
 ◊; emacs:
 ◊; Local Variables:
