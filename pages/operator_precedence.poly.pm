566bdedcd1b5dc298a47ca3c477820b63af99006
Peter Marinov <efravia@gmail.com>
2020-07-04 14:49:00 -0700

    chapter: Operator precedence (part 1)

---
+++ pages/operator_precedence.poly.pm
@@ -0,0 +1,22 @@
+#lang pollen
+
+◊define-meta[page-title]{Precedence}
+◊define-meta[page-description]{Operator Precedence}
+
+◊definition-block[#:type "code"]{
+◊definition-entry[#:name "var++ var--"]{
+post-increment, post-decrement
+}
+◊definition-entry[#:name "++var --var"]{
+pre-increment, pre-decrement
+}
+◊definition-entry[#:name "! ~"]{
+negation
+}
+
+} ◊; definition-block{}
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
