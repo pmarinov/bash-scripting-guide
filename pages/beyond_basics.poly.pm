7c2592b95a16f89e5c74291935181b54632a813b
Peter Marinov <efravia@gmail.com>
2020-07-26 17:53:36 -0700

    chapter: Beyond the Basics

---
+++ pages/beyond_basics.poly.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+◊define-meta[page-title]{Beyond Basics}
+◊define-meta[page-description]{Beyond the Basics}
+
+This chapter offers a deeper dive into variables and loops
+
+◊(node-menu "pages/beyond_basics.poly.pm")
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
