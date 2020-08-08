b9c41eb850cea6c706ad08d0aa95674e29c461a0
Peter Marinov <efravia@gmail.com>
2020-08-07 18:29:10 -0700

    chapter: Internal variables (Part 4)

--- pages/internal_variables.poly.pm
+++ pages/internal_variables.poly.pm
@@ -216,7 +216,153 @@ Host type
 
 Like ◊code{$MACHTYPE}, identifies the system hardware.
 
+◊example{
+bash$ echo $HOSTTYPE
+i686
 }
+
+}
+
+◊definition-entry[#:name "$IFS"]{
+internal field separator
+
+This variable determines how Bash recognizes fields, or word
+boundaries, when it interprets character strings.
+
+◊code{$IFS} defaults to whitespace (space, tab, and newline), but may
+be changed, for example, to parse a comma-separated data file. Note
+that ◊code{$*} uses the first character held in ◊code{$IFS}. See
+Example 5-1 (TODO)
+
+◊example{
+bash$ echo "$IFS"
+
+(With $IFS set to default, a blank line displays.)
+
+
+bash$ echo "$IFS" | cat -vte
+ ^I$
+ $
+(Show whitespace: here a single space, ^I [horizontal tab],
+  and newline, and display "$" at end-of-line.)
+
+
+bash$ bash -c 'set w x y z; IFS=":-;"; echo "$*"'
+w:x:y:z
+(Read commands from string and assign any arguments to pos params.)
+
+}
+
+Set ◊code{$IFS} to eliminate whitespace in pathnames.
+
+◊example{
+IFS="$(printf '\n\t')"   # Per David Wheeler.
+}
+
+Caution: ◊code{$IFS} does not handle whitespace the same as it does
+other characters.
+
+◊anchored-example[#:anchor "ifs_wspc1"]{Example: $IFS and whitespace}
+
+◊example{
+#!/bin/bash
+# ifs.sh
+
+
+var1="a+b+c"
+var2="d-e-f"
+var3="g,h,i"
+
+IFS=+
+# The plus sign will be interpreted as a separator.
+echo $var1     # a b c
+echo $var2     # d-e-f
+echo $var3     # g,h,i
+
+echo
+
+IFS="-"
+# The plus sign reverts to default interpretation.
+# The minus sign will be interpreted as a separator.
+echo $var1     # a+b+c
+echo $var2     # d e f
+echo $var3     # g,h,i
+
+echo
+
+IFS=","
+# The comma will be interpreted as a separator.
+# The minus sign reverts to default interpretation.
+echo $var1     # a+b+c
+echo $var2     # d-e-f
+echo $var3     # g h i
+
+echo
+
+IFS=" "
+# The space character will be interpreted as a separator.
+# The comma reverts to default interpretation.
+echo $var1     # a+b+c
+echo $var2     # d-e-f
+echo $var3     # g,h,i
+
+# ======================================================== #
+
+# However ...
+# $IFS treats whitespace differently than other characters.
+
+output_args_one_per_line()
+{
+  for arg
+  do
+    echo "[$arg]"
+  done #  ^    ^   Embed within brackets, for your viewing pleasure.
+}
+
+echo; echo "IFS=\" \""
+echo "-------"
+
+IFS=" "
+var=" a  b c   "
+#    ^ ^^   ^^^
+output_args_one_per_line $var  # output_args_one_per_line `echo " a  b c   "`
+# [a]
+# [b]
+# [c]
+
+
+echo; echo "IFS=:"
+echo "-----"
+
+IFS=:
+var=":a::b:c:::"               # Same pattern as above,
+#    ^ ^^   ^^^                #+ but substituting ":" for " "  ...
+output_args_one_per_line $var
+# []
+# [a]
+# []
+# [b]
+# [c]
+# []
+# []
+
+# Note "empty" brackets.
+# The same thing happens with the "FS" field separator in awk.
+
+
+echo
+
+exit
+}
+
+(Many thanks, Stéphane Chazelas, for clarification and above
+examples.)
+
+See also Example 16-41, Example 11-8, and Example 19-14 (TODO) for
+instructive examples of using ◊code{$IFS}.
+
+}
+
 
 } ◊; definition-block
 
