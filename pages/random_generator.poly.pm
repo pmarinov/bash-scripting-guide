9338991d94663fa9ecf5f0cc7d983d9c4714dafe
Peter Marinov <efravia@gmail.com>
2020-08-22 17:05:37 -0700

    chapter: 21093: generate random integer

---
+++ pages/random_generator.poly.pm
@@ -0,0 +1,387 @@
+#lang pollen
+
+◊define-meta[page-title]{$RANDOM}
+◊define-meta[page-description]{$RANDOM: generate random integer}
+
+◊quotation[#:author "John von Neumann"]{Anyone who attempts to
+generate random numbers by deterministic means is, of course, living
+in a state of sin.}
+
+◊code{$RANDOM} is an internal Bash function (not a constant) that
+returns a pseudorandom integer in the range 0 - 32767. It should
+not be used to generate an encryption key. ◊footnote{True
+"randomness," insofar as it exists at all, can only be found in
+certain incompletely understood natural phenomena, such as radioactive
+decay. Computers only simulate randomness, and computer-generated
+sequences of "random" numbers are therefore referred to as
+pseudorandom.}
+
+◊section-example[#:anchor "gen_rnd1"]{Generating random numbers}
+
+◊example{
+#!/bin/bash
+
+# $RANDOM returns a different random integer at each invocation.
+# Nominal range: 0 - 32767 (signed 16-bit integer).
+
+MAXCOUNT=10
+count=1
+
+echo
+echo "$MAXCOUNT random numbers:"
+echo "-----------------"
+while [ "$count" -le $MAXCOUNT ]      # Generate 10 ($MAXCOUNT) random integers.
+do
+  number=$RANDOM
+  echo $number
+  let "count += 1"  # Increment count.
+done
+echo "-----------------"
+
+# If you need a random int within a certain range, use the 'modulo' operator.
+# This returns the remainder of a division operation.
+
+RANGE=500
+
+echo
+
+number=$RANDOM
+let "number %= $RANGE"
+#           ^^
+echo "Random number less than $RANGE  ---  $number"
+
+echo
+
+
+
+#  If you need a random integer greater than a lower bound,
+#+ then set up a test to discard all numbers below that.
+
+FLOOR=200
+
+number=0   #initialize
+while [ "$number" -le $FLOOR ]
+do
+  number=$RANDOM
+done
+echo "Random number greater than $FLOOR ---  $number"
+echo
+
+   # Let's examine a simple alternative to the above loop, namely
+   #       let "number = $RANDOM + $FLOOR"
+   # That would eliminate the while-loop and run faster.
+   # But, there might be a problem with that. What is it?
+
+
+
+# Combine above two techniques to retrieve random number between two limits.
+number=0   #initialize
+while [ "$number" -le $FLOOR ]
+do
+  number=$RANDOM
+  let "number %= $RANGE"  # Scales $number down within $RANGE.
+done
+echo "Random number between $FLOOR and $RANGE ---  $number"
+echo
+
+
+
+# Generate binary choice, that is, "true" or "false" value.
+BINARY=2
+T=1
+number=$RANDOM
+
+let "number %= $BINARY"
+#  Note that    let "number >>= 14"    gives a better random distribution
+#+ (right shifts out everything except last binary digit).
+if [ "$number" -eq $T ]
+then
+  echo "TRUE"
+else
+  echo "FALSE"
+fi
+
+echo
+
+
+# Generate a toss of the dice.
+SPOTS=6   # Modulo 6 gives range 0 - 5.
+          # Incrementing by 1 gives desired range of 1 - 6.
+          # Thanks, Paulo Marcel Coelho Aragao, for the simplification.
+die1=0
+die2=0
+# Would it be better to just set SPOTS=7 and not add 1? Why or why not?
+
+# Tosses each die separately, and so gives correct odds.
+
+    let "die1 = $RANDOM % $SPOTS +1" # Roll first one.
+    let "die2 = $RANDOM % $SPOTS +1" # Roll second one.
+    #  Which arithmetic operation, above, has greater precedence --
+    #+ modulo (%) or addition (+)?
+
+
+let "throw = $die1 + $die2"
+echo "Throw of the dice = $throw"
+echo
+
+
+exit 0
+}
+
+◊section-example[#:anchor "rnd_card1"]{Picking a random card from a
+deck}
+
+◊example{
+#!/bin/bash
+# pick-card.sh
+
+# This is an example of choosing random elements of an array.
+
+
+# Pick a card, any card.
+
+Suites="Clubs
+Diamonds
+Hearts
+Spades"
+
+Denominations="2
+3
+4
+5
+6
+7
+8
+9
+10
+Jack
+Queen
+King
+Ace"
+
+# Note variables spread over multiple lines.
+
+
+suite=($Suites)                # Read into array variable.
+denomination=($Denominations)
+
+num_suites=${#suite[*]}        # Count how many elements.
+num_denominations=${#denomination[*]}
+
+echo -n "${denomination[$((RANDOM%num_denominations))]} of "
+echo ${suite[$((RANDOM%num_suites))]}
+
+
+# $bozo sh pick-cards.sh
+# Jack of Clubs
+
+
+# Thank you, "jipe," for pointing out this use of $RANDOM.
+exit 0
+}
+
+◊section-example[#:anchor "brwn_mo1"]{Brownian Motion Simulation}
+
+◊example{
+#!/bin/bash
+# How random is RANDOM?
+
+RANDOM=$$       # Reseed the random number generator using script process ID.
+
+PIPS=6          # A die has 6 pips.
+MAXTHROWS=600   # Increase this if you have nothing better to do with your time.
+throw=0         # Number of times the dice have been cast.
+
+ones=0          #  Must initialize counts to zero,
+twos=0          #+ since an uninitialized variable is null, NOT zero.
+threes=0
+fours=0
+fives=0
+sixes=0
+
+print_result ()
+{
+echo
+echo "ones =   $ones"
+echo "twos =   $twos"
+echo "threes = $threes"
+echo "fours =  $fours"
+echo "fives =  $fives"
+echo "sixes =  $sixes"
+echo
+}
+
+update_count()
+{
+case "$1" in
+  0) ((ones++));;   # Since a die has no "zero", this corresponds to 1.
+  1) ((twos++));;   # And this to 2.
+  2) ((threes++));; # And so forth.
+  3) ((fours++));;
+  4) ((fives++));;
+  5) ((sixes++));;
+esac
+}
+
+echo
+
+
+while [ "$throw" -lt "$MAXTHROWS" ]
+do
+  let "die1 = RANDOM % $PIPS"
+  update_count $die1
+  let "throw += 1"
+done
+
+print_result
+
+exit $?
+
+#  The scores should distribute evenly, assuming RANDOM is random.
+#  With $MAXTHROWS at 600, all should cluster around 100,
+#+ plus-or-minus 20 or so.
+#
+#  Keep in mind that RANDOM is a ***pseudorandom*** generator,
+#+ and not a spectacularly good one at that.
+
+#  Randomness is a deep and complex subject.
+#  Sufficiently long "random" sequences may exhibit
+#+ chaotic and other "non-random" behavior.
+
+# Exercise (easy):
+# ---------------
+# Rewrite this script to flip a coin 1000 times.
+# Choices are "HEADS" and "TAILS."
+}
+
+As we have seen in the last example, it is best to reseed the
+◊code{RANDOM} generator each time it is invoked. Using the same seed
+for ◊code{RANDOM} repeats the same series of numbers. (This mirrors
+the behavior of the ◊code{random()} function in C.) ◊footnote{The seed
+of a computer-generated pseudorandom number series can be considered
+an identification label. For example, think of the pseudorandom series
+with a seed of 23 as Series #23.
+
+A property of a pseurandom number series is the length of the cycle
+before it starts repeating itself. A good pseurandom generator will
+produce series with very long cycles.}
+
+◊section-example[#:anchor "reset_rnd1"]{Reseeding RANDOM}
+
+◊example{
+#!/bin/bash
+# seeding-random.sh: Seeding the RANDOM variable.
+# v 1.1, reldate 09 Feb 2013
+
+MAXCOUNT=25       # How many numbers to generate.
+SEED=
+
+random_numbers ()
+{
+local count=0
+local number
+
+while [ "$count" -lt "$MAXCOUNT" ]
+do
+  number=$RANDOM
+  echo -n "$number "
+  let "count++"
+done
+}
+
+echo; echo
+
+SEED=1
+RANDOM=$SEED      # Setting RANDOM seeds the random number generator.
+echo "Random seed = $SEED"
+random_numbers
+
+
+RANDOM=$SEED      # Same seed for RANDOM . . .
+echo; echo "Again, with same random seed ..."
+echo "Random seed = $SEED"
+random_numbers    # . . . reproduces the exact same number series.
+                  #
+                  # When is it useful to duplicate a "random" series?
+
+echo; echo
+
+SEED=2
+RANDOM=$SEED      # Trying again, but with a different seed . . .
+echo "Random seed = $SEED"
+random_numbers    # . . . gives a different number series.
+
+echo; echo
+
+# RANDOM=$$  seeds RANDOM from process id of script.
+# It is also possible to seed RANDOM from 'time' or 'date' commands.
+
+# Getting fancy...
+SEED=$(head -1 /dev/urandom | od -N 1 | awk '{ print $2 }'| sed s/^0*//)
+#  Pseudo-random output fetched
+#+ from /dev/urandom (system pseudo-random device-file),
+#+ then converted to line of printable (octal) numbers by "od",
+#+ then "awk" retrieves just one number for SEED,
+#+ finally "sed" removes any leading zeros.
+RANDOM=$SEED
+echo "Random seed = $SEED"
+random_numbers
+
+echo; echo
+
+exit 0
+}
+
+Note: The ◊fname{/dev/urandom} pseudo-device file provides a method of
+generating much more "random" pseudorandom numbers than the
+◊code{$RANDOM} variable. ◊command{dd if=/dev/urandom of=targetfile
+bs=1 count=XX} creates a file of well-scattered pseudorandom
+numbers. However, assigning these numbers to a variable in a script
+requires a workaround, such as filtering through ◊code{od} (as in
+above example, Example 16-14, and Example A-36), or even piping to
+◊code{md5sum} (see Example 36-16). (TODO)
+
+◊section-example[#:anchor "rnd_awk1"]{Pseudorandom numbers, using awk}
+
+There are also other ways to generate pseudorandom numbers in a
+script. ◊code{Awk} provides a convenient means of doing this.
+
+◊example{
+#!/bin/bash
+#  random2.sh: Returns a pseudorandom number in the range 0 - 1,
+#+ to 6 decimal places. For example: 0.822725
+#  Uses the awk rand() function.
+
+AWKSCRIPT=' { srand(); print rand() } '
+#           Command(s)/parameters passed to awk
+# Note that srand() reseeds awk's random number generator.
+
+
+echo -n "Random number between 0 and 1 = "
+
+echo | awk "$AWKSCRIPT"
+# What happens if you leave out the 'echo'?
+
+exit 0
+
+
+# Exercises:
+# ---------
+
+# 1) Using a loop construct, print out 10 different random numbers.
+#      (Hint: you must reseed the srand() function with a different seed
+#+     in each pass through the loop. What happens if you omit this?)
+
+# 2) Using an integer multiplier as a scaling factor, generate random numbers
+#+   in the range of 10 to 100.
+
+# 3) Same as exercise #2, above, but generate random integers this time.
+}
+
+The ◊command{date} command also lends itself to generating
+pseudorandom integer sequences.
+
+◊; emacs:
+◊; Local Variables:
+◊; mode: fundamental
+◊; End:
