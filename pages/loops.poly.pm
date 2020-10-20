7aca93ed13ddd387127b42209f281e86e96b61ee
Peter Marinov <efravia@gmail.com>
2020-10-19 19:54:24 -0700

    chapter: Loops (part 2)

--- pages/loops.poly.pm
+++ pages/loops.poly.pm
@@ -152,6 +152,75 @@ do
 done
 }
 
+◊section-example[#:anchor "files_forloo1"]{Operating on files with a
+for loop}
+
+If the ◊code{[list]} in a ◊code{for} loop contains wild cards
+(◊code{*} and ◊code{?}) used in filename expansion, then globbing
+takes place.
+
+◊example{
+#!/bin/bash
+# list-glob.sh: Generating [list] in a for-loop, using "globbing" ...
+# Globbing = filename expansion.
+
+echo
+
+for file in *
+#           ^  Bash performs filename expansion
+#+             on expressions that globbing recognizes.
+do
+  ls -l "$file"  # Lists all files in $PWD (current directory).
+  #  Recall that the wild card character "*" matches every filename,
+  #+ however, in "globbing," it doesn't match dot-files.
+
+  #  If the pattern matches no file, it is expanded to itself.
+  #  To prevent this, set the nullglob option
+  #+   (shopt -s nullglob).
+  #  Thanks, S.C.
+done
+
+echo; echo
+
+for file in [jx]*
+do
+  rm -f $file    # Removes only files beginning with "j" or "x" in $PWD.
+  echo "Removed file \"$file\"".
+done
+
+echo
+
+exit 0
+}
+
+◊section-example[#:anchor "for_missing_lst1"]{Missing in [list] in a
+for loop}
+
+Omitting the ◊code{in [list]} part of a ◊code{for} loop causes the
+loop to operate on ◊code{$◊escaped{@}} -- the positional parameters. A
+particularly clever illustration of this is TODO Example A-15. See
+also Example 15-17.
+
+◊example{
+#!/bin/bash
+
+#  Invoke this script both with and without arguments,
+#+ and see what happens.
+
+for a
+do
+ echo -n "$a "
+done
+
+#  The 'in list' missing, therefore the loop operates on '$@'
+#+ (command-line argument list, including whitespace).
+
+echo
+
+exit 0
+}
+
+
 ◊; emacs:
 ◊; Local Variables:
 ◊; mode: fundamental
