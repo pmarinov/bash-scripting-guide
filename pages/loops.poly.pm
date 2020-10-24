d1119cc144a6caec62700303303bce64815339b5
Peter Marinov <efravia@gmail.com>
2020-10-23 19:52:33 -0700

    chapter: Loops (part 4)

--- pages/loops.poly.pm
+++ pages/loops.poly.pm
@@ -382,6 +382,270 @@ done
 ◊section-example[#:anchor "ls_syml1"]{Listing the symbolic links in a
 directory}
 
+The output of a ◊code{for} loop may be piped to a command or commands.
+
+◊example{
+#!/bin/bash
+# symlinks.sh: Lists symbolic links in a directory.
+
+
+directory=${1-`pwd`}
+#  Defaults to current working directory,
+#+ if not otherwise specified.
+#  Equivalent to code block below.
+# ----------------------------------------------------------
+# ARGS=1                 # Expect one command-line argument.
+#
+# if [ $# -ne "$ARGS" ]  # If not 1 arg...
+# then
+#   directory=`pwd`      # current working directory
+# else
+#   directory=$1
+# fi
+# ----------------------------------------------------------
+
+echo "symbolic links in directory \"$directory\""
+
+for file in "$( find $directory -type l )"   # -type l = symbolic links
+do
+  echo "$file"
+done | sort                                  # Otherwise file list is unsorted.
+#  Strictly speaking, a loop isn't really necessary here,
+#+ since the output of the "find" command is expanded into a single word.
+#  However, it's easy to understand and illustrative this way.
+
+#  As Dominik 'Aeneas' Schnitzer points out,
+#+ failing to quote  $( find $directory -type l )
+#+ will choke on filenames with embedded whitespace.
+#  containing whitespace. 
+
+exit 0
+
+
+# --------------------------------------------------------
+# Jean Helou proposes the following alternative:
+
+echo "symbolic links in directory \"$directory\""
+# Backup of the current IFS. One can never be too cautious.
+OLDIFS=$IFS
+IFS=:
+
+for file in $(find $directory -type l -printf "%p$IFS")
+do     #                              ^^^^^^^^^^^^^^^^
+       echo "$file"
+       done|sort
+
+# And, James "Mike" Conley suggests modifying Helou's code thusly:
+
+OLDIFS=$IFS
+IFS='' # Null IFS means no word breaks
+for file in $( find $directory -type l )
+do
+  echo $file
+  done | sort
+
+#  This works in the "pathological" case of a directory name having
+#+ an embedded colon.
+#  "This also fixes the pathological case of the directory name having
+#+  a colon (or space in earlier example) as well."
+}
+
+◊section-example[#:anchor "for_lnk_f1"]{Symbolic links in a directory,
+saved to a file}
+
+The ◊code{stdout} of a loop may be redirected to a file, as this
+slight modification to the previous example shows.
+
+◊example{
+#!/bin/bash
+# symlinks.sh: Lists symbolic links in a directory.
+
+OUTFILE=symlinks.list                         # save-file
+
+directory=${1-`pwd`}
+#  Defaults to current working directory,
+#+ if not otherwise specified.
+
+
+echo "symbolic links in directory \"$directory\"" > "$OUTFILE"
+echo "---------------------------" >> "$OUTFILE"
+
+for file in "$( find $directory -type l )"    # -type l = symbolic links
+do
+  echo "$file"
+done | sort >> "$OUTFILE"                     # stdout of loop
+#           ^^^^^^^^^^^^^                       redirected to save file.
+
+# echo "Output file = $OUTFILE"
+
+exit $?
+}
+
+◊section-example[#:anchor "for_cstyle1"]{A C-style for loop}
+
+There is an alternative syntax to a ◊code{for} loop that will look
+very familiar to C programmers. This requires double parentheses.
+
+◊example{
+#!/bin/bash
+# Multiple ways to count up to 10.
+
+echo
+
+# Standard syntax.
+for a in 1 2 3 4 5 6 7 8 9 10
+do
+  echo -n "$a "
+done  
+
+echo; echo
+
+# +==========================================+
+
+# Using "seq" ...
+for a in `seq 10`
+do
+  echo -n "$a "
+done  
+
+echo; echo
+
+# +==========================================+
+
+# Using brace expansion ...
+# Bash, version 3+.
+for a in {1..10}
+do
+  echo -n "$a "
+done  
+
+echo; echo
+
+# +==========================================+
+
+# Now, let's do the same, using C-like syntax.
+
+LIMIT=10
+
+for ((a=1; a <= LIMIT ; a++))  # Double parentheses, and naked "LIMIT"
+do
+  echo -n "$a "
+done                           # A construct borrowed from ksh93.
+
+echo; echo
+
+# +=========================================================================+
+
+# Let's use the C "comma operator" to increment two variables simultaneously.
+
+for ((a=1, b=1; a <= LIMIT ; a++, b++))
+do  # The comma concatenates operations.
+  echo -n "$a-$b "
+done
+
+echo; echo
+
+exit 0
+}
+
+See also TODO Example 27-16, Example 27-17, and Example A-6.
+
+◊section-example[#:anchor "for_efax1"]{Using efax in batch mode}
+
+Now, a for loop used in a "real-life" context.
+
+◊example{
+#!/bin/bash
+# Faxing (must have 'efax' package installed).
+
+EXPECTED_ARGS=2
+E_BADARGS=85
+MODEM_PORT="/dev/ttyS2"   # May be different on your machine.
+#                ^^^^^      PCMCIA modem card default port.
+
+if [ $# -ne $EXPECTED_ARGS ]
+# Check for proper number of command-line args.
+then
+   echo "Usage: `basename $0` phone# text-file"
+   exit $E_BADARGS
+fi
+
+
+if [ ! -f "$2" ]
+then
+  echo "File $2 is not a text file."
+  #     File is not a regular file, or does not exist.
+  exit $E_BADARGS
+fi
+  
+
+fax make $2              #  Create fax-formatted files from text files.
+
+for file in $(ls $2.0*)  #  Concatenate the converted files.
+                         #  Uses wild card (filename "globbing")
+			 #+ in variable list.
+do
+  fil="$fil $file"
+done  
+
+efax -d "$MODEM_PORT"  -t "T$1" $fil   # Finally, do the work.
+# Trying adding  -o1  if above line fails.
+
+
+#  As S.C. points out, the for-loop can be eliminated with
+#     efax -d /dev/ttyS2 -o1 -t "T$1" $2.0*
+#+ but it's not quite as instructive [grin].
+
+exit $?   # Also, efax sends diagnostic messages to stdout.
+}
+
+Note: The keywords ◊code{do} and ◊code{done} delineate the ◊code{for}
+loop command block. However, these may, in certain contexts, be
+omitted by framing the command block within curly brackets
+
+◊example{
+for((n=1; n<=10; n++)) 
+# No do!
+{
+  echo -n "* $n *"
+}
+# No done!
+
+
+# Outputs:
+# * 1 ** 2 ** 3 ** 4 ** 5 ** 6 ** 7 ** 8 ** 9 ** 10 *
+# And, echo $? returns 0, so Bash does not register an error.
+
+
+echo
+
+
+#  But, note that in a classic for-loop:    for n in [list] ...
+#+ a terminal semicolon is required.
+
+for n in 1 2 3
+{  echo -n "$n "; }
+#               ^
+
+
+# Thank you, YongYe, for pointing this out.
+}
+
+◊section{while}
+
+This construct tests for a condition at the top of a loop, and keeps
+looping as long as that condition is true (returns a 0 exit
+status). In contrast to a ◊code{for} loop, a ◊code{while} loop finds
+use in situations where the number of loop repetitions is not known
+beforehand.
+
+◊example{
+while [ condition ]
+do
+ command(s)...
+done
+}
+
 ◊; emacs:
 ◊; Local Variables:
 ◊; mode: fundamental
