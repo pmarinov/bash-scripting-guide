2ea4904849525177cf862027b02857bc60b22c54
Peter Marinov <efravia@gmail.com>
2020-11-12 19:39:24 -0800

    chapter: Command Substitution (part 3)

--- pages/command_substit.poly.pm
+++ pages/command_substit.poly.pm
@@ -34,7 +34,7 @@ loop.
 
 Note: Command substitution invokes a subshell.
 
-◊section{Use examples}
+◊section{Usage examples}
 
 ◊example{
 rm `cat filename`   # "filename" contains a list of files to delete.
@@ -153,6 +153,78 @@ output=$(sed -n /"$1"/p $file)   # From "grp.sh"	example.
 # Setting a variable to the contents of a text file.
 File_contents1=$(cat $file1)
 File_contents2=$(<$file2)        # Bash permits this also.
+}
+
+The ◊code{$(...)} form of command substitution treats a double backslash in a
+different way than ◊code{`...`}.
+
+◊example{
+bash$ echo `echo \\`
+
+
+bash$ echo $(echo \\)
+\
+}
+
+The ◊code{$(...)} form of command substitution permits
+nesting. ◊footnote{In fact, nesting with backticks is also possible,
+but only by escaping the inner backticks, as John Default points
+out. ◊code{word_count=` wc -w \`echo * | awk '◊escaped{"{"}print
+$8◊escaped{"}"}'\` `}}
+
+◊example{
+word_count=$( wc -w $(echo * | awk '{print $8}') )
+}
+
+◊section-example[#:anchor "finding_anagrams1"]{Finding anagrams}
+
+◊example{
+#!/bin/bash
+# agram2.sh
+# Example of nested command substitution.
+
+#  Uses "anagram" utility
+#+ that is part of the author's "yawl" word list package.
+#  http://ibiblio.org/pub/Linux/libs/yawl-0.3.2.tar.gz
+#  http://bash.deta.in/yawl-0.3.2.tar.gz
+
+E_NOARGS=86
+E_BADARG=87
+MINLEN=7
+
+if [ -z "$1" ]
+then
+  echo "Usage $0 LETTERSET"
+  exit $E_NOARGS         # Script needs a command-line argument.
+elif [ ${#1} -lt $MINLEN ]
+then
+  echo "Argument must have at least $MINLEN letters."
+  exit $E_BADARG
+fi
+
+
+
+FILTER='.......'         # Must have at least 7 letters.
+#       1234567
+Anagrams=( $(echo $(anagram $1 | grep $FILTER) ) )
+#          $(     $(  nested command sub.    ) )
+#        (              array assignment         )
+
+echo
+echo "${#Anagrams[*]}  7+ letter anagrams found"
+echo
+echo ${Anagrams[0]}      # First anagram.
+echo ${Anagrams[1]}      # Second anagram.
+                         # Etc.
+
+# echo "${Anagrams[*]}"  # To list all the anagrams in a single line . . .
+
+#  Look ahead to the Arrays chapter for enlightenment on
+#+ what's going on here.
+
+# See also the agram.sh script for an exercise in anagram finding.
+
+exit $?
 }
 
 ◊section{What to avoid}
