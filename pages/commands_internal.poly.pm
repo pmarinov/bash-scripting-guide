1d1cf84ee0b3f2971654530b775c6044807e60f9
Peter Marinov <efravia@gmail.com>
2020-11-25 19:38:18 -0800

    chapter: Internal Commands and Builtins (part 6)

--- pages/commands_internal.poly.pm
+++ pages/commands_internal.poly.pm
@@ -1127,7 +1127,133 @@ is to reset the positional parameters that a script sees as the result
 of a command (◊code{set `command`}). The script can then parse the
 fields of the command output.
 
+◊example{
+#!/bin/bash
+# ex34.sh
+# Script "set-test"
+
+# Invoke this script with three command-line parameters,
+# for example, "sh ex34.sh one two three".
+
+echo
+echo "Positional parameters before  set \`uname -a\` :"
+echo "Command-line argument #1 = $1"
+echo "Command-line argument #2 = $2"
+echo "Command-line argument #3 = $3"
+
+
+set `uname -a` # Sets the positional parameters to the output
+               # of the command `uname -a`
+
+echo
+echo +++++
+echo $_        # +++++
+# Flags set in script.
+echo $-        # hB
+#                Anomalous behavior?
+echo
+
+echo "Positional parameters after  set \`uname -a\` :"
+# $1, $2, $3, etc. reinitialized to result of `uname -a`
+echo "Field #1 of 'uname -a' = $1"
+echo "Field #2 of 'uname -a' = $2"
+echo "Field #3 of 'uname -a' = $3"
+echo \#\#\#
+echo $_        # ###
+echo
+
+exit 0
 }
+
+◊anchored-example[#:anchor "set_pos2"]{Reversing the positional parameters}
+
+More fun with positional parameters.
+
+◊example{
+#!/bin/bash
+# revposparams.sh: Reverse positional parameters.
+# Script by Dan Jacobson, with stylistic revisions by document author.
+
+
+set a\ b c d\ e;
+#     ^      ^     Spaces escaped 
+#       ^ ^        Spaces not escaped
+OIFS=$IFS; IFS=:;
+#              ^   Saving old IFS and setting new one.
+
+echo
+
+until [ $# -eq 0 ]
+do          #      Step through positional parameters.
+  echo "### k0 = "$k""     # Before
+  k=$1:$k;  #      Append each pos param to loop variable.
+#     ^
+  echo "### k = "$k""      # After
+  echo
+  shift;
+done
+
+set $k  #  Set new positional parameters.
+echo -
+echo $# #  Count of positional parameters.
+echo -
+echo
+
+for i   #  Omitting the "in list" sets the variable -- i --
+        #+ to the positional parameters.
+do
+  echo $i  # Display new positional parameters.
+done
+
+IFS=$OIFS  # Restore IFS.
+
+#  Question:
+#  Is it necessary to set an new IFS, internal field separator,
+#+ in order for this script to work properly?
+#  What happens if you don't? Try it.
+#  And, why use the new IFS -- a colon -- in line 17,
+#+ to append to the loop variable?
+#  What is the purpose of this?
+
+exit 0
+
+$ ./revposparams.sh
+
+### k0 = 
+### k = a b
+
+### k0 = a b
+### k = c a b
+
+### k0 = c a b
+### k = d e c a b
+
+-
+3
+-
+
+d e
+c
+a b
+}
+
+Invoking ◊command{set} without any options or arguments simply lists
+all the environmental and other variables that have been initialized.
+
+◊example{
+bash$ set
+AUTHORCOPY=/home/bozo/posts
+ BASH=/bin/bash
+ BASH_VERSION=$'2.05.8(1)-release'
+ ...
+ XAUTHORITY=/home/bozo/.Xauthority
+ _=/etc/bashrc
+ variable22=abc
+ variable23=xzy
+}
+
+}
+
 
 }
 
