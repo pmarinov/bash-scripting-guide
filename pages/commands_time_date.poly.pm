43dfe748b7f117549640c29f5491bb5a38028810
Peter Marinov <efravia@gmail.com>
2021-01-15 19:44:59 -0800

    chapter: Time / Date Commands (part 2)

--- pages/commands_time_date.poly.pm
+++ pages/commands_time_date.poly.pm
@@ -39,6 +39,108 @@ echo "Temporary filename = $filename"
 exit 0
 }
 
+The ◊code{-u} option gives the UTC (Universal Coordinated Time).
+
+◊example{
+bash$ date
+Fri Mar 29 21:07:39 MST 2002
+
+
+bash$ date -u
+Sat Mar 30 04:07:42 UTC 2002
+}
+
+This option facilitates calculating the time between different dates.
+
+◊example{
+#!/bin/bash
+# date-calc.sh
+# Author: Nathan Coulter
+# Used in ABS Guide with permission (thanks!).
+
+MPHR=60    # Minutes per hour.
+HPD=24     # Hours per day.
+
+diff () {
+        printf '%s' $(( $(date -u -d"$TARGET" +%s) -
+                        $(date -u -d"$CURRENT" +%s)))
+#                       %d = day of month.
+}
+
+
+CURRENT=$(date -u -d '2007-09-01 17:30:24' '+%F %T.%N %Z')
+TARGET=$(date -u -d'2007-12-25 12:30:00' '+%F %T.%N %Z')
+# %F = full date, %T = %H:%M:%S, %N = nanoseconds, %Z = time zone.
+
+printf '\nIn 2007, %s ' \
+       "$(date -d"$CURRENT +
+        $(( $(diff) /$MPHR /$MPHR /$HPD / 2 )) days" '+%d %B')" 
+#       %B = name of month                ^ halfway
+printf 'was halfway between %s ' "$(date -d"$CURRENT" '+%d %B')"
+printf 'and %s\n' "$(date -d"$TARGET" '+%d %B')"
+
+printf '\nOn %s at %s, there were\n' \
+        $(date -u -d"$CURRENT" +%F) $(date -u -d"$CURRENT" +%T)
+DAYS=$(( $(diff) / $MPHR / $MPHR / $HPD ))
+CURRENT=$(date -d"$CURRENT +$DAYS days" '+%F %T.%N %Z')
+HOURS=$(( $(diff) / $MPHR / $MPHR ))
+CURRENT=$(date -d"$CURRENT +$HOURS hours" '+%F %T.%N %Z')
+MINUTES=$(( $(diff) / $MPHR ))
+CURRENT=$(date -d"$CURRENT +$MINUTES minutes" '+%F %T.%N %Z')
+printf '%s days, %s hours, ' "$DAYS" "$HOURS"
+printf '%s minutes, and %s seconds ' "$MINUTES" "$(diff)"
+printf 'until Christmas Dinner!\n\n'
+
+#  Exercise:
+#  --------
+#  Rewrite the diff () function to accept passed parameters,
+#+ rather than using global variables.
+}
+
+The ◊command{date} command has quite a number of output options. For
+example ◊code{%N} gives the nanosecond portion of the current
+time. One interesting use for this is to generate random integers.
+
+◊example{
+date +%N | sed -e 's/000$//' -e 's/^0//'
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#  Strip off leading and trailing zeroes, if present.
+#  Length of generated integer depends on
+#+ how many zeroes stripped off.
+
+# 115281032
+# 63408725
+# 394504284
+}
+
+There are many more options (try ◊code{man date}).
+
+◊example{
+date +%j
+# Echoes day of the year (days elapsed since January 1).
+
+date +%k%M
+# Echoes hour and minute in 24-hour format, as a single digit string.
+
+
+
+# The 'TZ' parameter permits overriding the default time zone.
+date                 # Mon Mar 28 21:42:16 MST 2005
+TZ=EST date          # Mon Mar 28 23:42:16 EST 2005
+# Thanks, Frank Kannemann and Pete Sjoberg, for the tip.
+
+
+SixDaysAgo=$(date --date='6 days ago')
+OneMonthAgo=$(date --date='1 month ago')  # Four weeks back (not a month!)
+OneYearAgo=$(date --date='1 year ago')
+}
+
+See also TODO Example 3-4 and Example A-43.
+
+}
+
+◊definition-entry[#:name "zdump"]{
+
 }
 
 }
