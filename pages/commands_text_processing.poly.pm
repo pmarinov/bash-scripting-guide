90515d89907a9fef51a8a12bacd03243a9f093b1
Peter Marinov <efravia@gmail.com>
2021-01-31 13:08:50 -0800

    chapter: Text Processingf Commands (part 2)

--- pages/commands_text_processing.poly.pm
+++ pages/commands_text_processing.poly.pm
@@ -30,6 +30,135 @@ those of the standard ◊command{sort} command, above.
 
 }
 
+◊definition-entry[#:name "uniq"]{
+This filter removes duplicate lines from a sorted file. It is often
+seen in a pipe coupled with ◊command{sort}.
+
+◊example{
+cat list-1 list-2 list-3 | sort | uniq > final.list
+# Concatenates the list files,
+# sorts them,
+# removes duplicate lines,
+# and finally writes the result to an output file.
+}
+
+The useful ◊code{-c} option prefixes each line of the input file with
+its number of occurrences.
+
+◊example{
+bash$ cat testfile
+This line occurs only once.
+ This line occurs twice.
+ This line occurs twice.
+ This line occurs three times.
+ This line occurs three times.
+ This line occurs three times.
+
+
+bash$ uniq -c testfile
+      1 This line occurs only once.
+      2 This line occurs twice.
+      3 This line occurs three times.
+
+
+bash$ sort testfile | uniq -c | sort -nr
+      3 This line occurs three times.
+      2 This line occurs twice.
+      1 This line occurs only once.
+}
+
+◊anchored-example[#:anchor "wrd_cnt1"]{Word Frequency Analysis}
+
+The ◊command{sort INPUTFILE | uniq -c | sort -nr} command string
+produces a frequency of occurrence listing on the INPUTFILE file (the
+◊code{-nr} options to sort cause a reverse numerical sort). This
+template finds use in analysis of log files and dictionary lists, and
+wherever the lexical structure of a document needs to be examined.
+
+◊example{
+#!/bin/bash
+# wf.sh: Crude word frequency analysis on a text file.
+# This is a more efficient version of the "wf2.sh" script.
+
+
+# Check for input file on command-line.
+ARGS=1
+E_BADARGS=85
+E_NOFILE=86
+
+if [ $# -ne "$ARGS" ]  # Correct number of arguments passed to script?
+then
+  echo "Usage: `basename $0` filename"
+  exit $E_BADARGS
+fi
+
+if [ ! -f "$1" ]       # Check if file exists.
+then
+  echo "File \"$1\" does not exist."
+  exit $E_NOFILE
+fi
+
+
+########################################################
+# main ()
+sed -e 's/\.//g'  -e 's/\,//g' -e 's/ /\
+/g' "$1" | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr
+#                           =========================
+#                            Frequency of occurrence
+
+#  Filter out periods and commas, and
+#+ change space between words to linefeed,
+#+ then shift characters to lowercase, and
+#+ finally prefix occurrence count and sort numerically.
+
+#  Arun Giridhar suggests modifying the above to:
+#  . . . | sort | uniq -c | sort +1 [-f] | sort +0 -nr
+#  This adds a secondary sort key, so instances of
+#+ equal occurrence are sorted alphabetically.
+#  As he explains it:
+#  "This is effectively a radix sort, first on the
+#+ least significant column
+#+ (word or string, optionally case-insensitive)
+#+ and last on the most significant column (frequency)."
+#
+#  As Frank Wang explains, the above is equivalent to
+#+       . . . | sort | uniq -c | sort +0 -nr
+#+ and the following also works:
+#+       . . . | sort | uniq -c | sort -k1nr -k
+########################################################
+
+exit 0
+
+# Exercises:
+# ---------
+# 1) Add 'sed' commands to filter out other punctuation,
+#+   such as semicolons.
+# 2) Modify the script to also filter out multiple spaces and
+#+   other whitespace.
+}
+
+◊example{
+bash$ cat testfile
+This line occurs only once.
+This line occurs twice.
+This line occurs twice.
+This line occurs three times.
+This line occurs three times.
+This line occurs three times.
+
+bash$ ./wf.sh testfile
+      6 this
+      6 occurs
+      6 line
+      3 times
+      3 three
+      2 twice
+      1 only
+      1 once
+}
+
+}
+
 }
 
 ◊; emacs:
