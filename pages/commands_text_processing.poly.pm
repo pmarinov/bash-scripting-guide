bf9bbb60c327af1ece2cf58c214c28b0cfc2ea82
Peter Marinov <efravia@gmail.com>
2021-02-27 19:21:28 -0800

    chapter: Text Processing Commands (part 14)

--- pages/commands_text_processing.poly.pm
+++ pages/commands_text_processing.poly.pm
@@ -1403,6 +1403,33 @@ non-blank line. If ◊fname{filename} omitted, operates on ◊code{stdin}.
 The output of ◊command{nl} is very similar to ◊command{cat -b}, since,
 by default ◊command{nl} does not list blank lines.
 
+◊anchored-example[#:anchor "nl1"]{nl: A self-numbering script}
+
+◊example{
+#!/bin/bash
+# line-number.sh
+
+# This script echoes itself twice to stdout with its lines numbered.
+
+echo "     line number = $LINENO" # 'nl' sees this as line 4
+#                                   (nl does not number blank lines).
+#                                   'cat -n' sees it correctly as line #6.
+
+nl `basename $0`
+
+echo; echo  # Now, let's try it with 'cat -n'
+
+cat -n `basename $0`
+# The difference is that 'cat -n' numbers the blank lines.
+# Note that 'nl -ba' will also do so.
+
+exit 0
+# -----------------------------------------------------------------
+}
+
+}
+
+◊definition-entry[#:name "pr"]{
 }
 
 }
