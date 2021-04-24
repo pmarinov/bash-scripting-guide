a0930935abbbdeb6d4e0e6643d7463063ccd13e7
Peter Marinov <efravia@gmail.com>
2021-04-24 16:36:33 -0700

    chapter: File and Archive Commands (part 7)

--- pages/commands_file_and_archiving.poly.pm
+++ pages/commands_file_and_archiving.poly.pm
@@ -334,11 +334,155 @@ programs.
 Highly efficient Lempel-Ziv-Markov compression. The syntax of ◊command{lzma} is
 similar to that of ◊command{gzip}.
 
-See: https://www.7-zip.org/sdk.html
+See: ◊url[#:link "https://www.7-zip.org/sdk.html"]{}
+
+}
+
+◊definition-entry[#:name "xz, unxz, xzcat"]{
+A new high-efficiency compression tool, backward compatible with ◊command{lzma},
+and with an invocation syntax similar to ◊command{gzip}.
+
+See: ◊url[#:link "https://en.wikipedia.org/wiki/XZ_Utils"]{}
 
 }
 
 }
+
+◊section{File Information}
+
+◊definition-block[#:type "code"]{
+
+◊definition-entry[#:name "file"]{
+
+A utility for identifying file types. The command ◊command{file
+file-name} will return a file specification for ◊code{file-name}, such
+as ascii text or data. It references the magic numbers found in
+◊fname{/usr/share/magic}, ◊fname{/etc/magic}, or
+◊fname{/usr/lib/magic}, depending on the Linux/UNIX distribution
+(consult ◊command{man file} for the exact location).
+
+The ◊code{-f} option causes file to run in batch mode, to read from a
+designated file a list of filenames to analyze. The ◊code{-z} option,
+when used on a compressed target file, forces an attempt to analyze
+the uncompressed file type.
+
+◊example{
+bash$ file test.tar.gz
+test.tar.gz: gzip compressed data, deflated,
+last modified: Sun Sep 16 13:34:51 2001, os: Unix
+
+bash file -z test.tar.gz
+test.tar.gz: GNU tar archive (gzip compressed data, deflated,
+last modified: Sun Sep 16 13:34:51 2001, os: Unix)
+}
+
+◊example{
+# Find sh and Bash scripts in a given directory:
+
+DIRECTORY=/usr/local/bin
+KEYWORD=Bourne
+# Bourne and Bourne-Again shell scripts
+
+file $DIRECTORY/* | fgrep $KEYWORD
+
+# Output:
+
+# /usr/local/bin/burn-cd:          Bourne-Again shell script text executable
+# /usr/local/bin/burnit:           Bourne-Again shell script text executable
+# /usr/local/bin/cassette.sh:      Bourne shell script text executable
+# /usr/local/bin/copy-cd:          Bourne-Again shell script text executable
+# . . .
+}
+
+◊anchored-example[#:anchor "file_strip_c1"]{Stripping comments from C
+program files}
+
+◊example{
+#!/bin/bash
+# strip-comment.sh: Strips out the comments (/* COMMENT */) in a C program.
+
+E_NOARGS=0
+E_ARGERROR=66
+E_WRONG_FILE_TYPE=67
+
+if [ $# -eq "$E_NOARGS" ]
+then
+  echo "Usage: `basename $0` C-program-file" >&2 # Error message to stderr.
+  exit $E_ARGERROR
+fi
+
+# Test for correct file type.
+type=`file $1 | awk '{ print $2, $3, $4, $5 }'`
+# "file $1" echoes file type . . .
+# Then awk removes the first field, the filename . . .
+# Then the result is fed into the variable "type."
+correct_type="ASCII C program text"
+
+if [ "$type" != "$correct_type" ]
+then
+  echo
+  echo "This script works on C program files only."
+  echo
+  exit $E_WRONG_FILE_TYPE
+fi
+
+
+# Rather cryptic sed script:
+#--------
+sed '
+/^\/\*/d
+/.*\*\//d
+' $1
+#--------
+# Easy to understand if you take several hours to learn sed fundamentals.
+
+
+#  Need to add one more line to the sed script to deal with
+#+ case where line of code has a comment following it on same line.
+#  This is left as a non-trivial exercise.
+
+#  Also, the above code deletes non-comment lines with a "*/" . . .
+#+ not a desirable result.
+
+exit 0
+
+
+# ----------------------------------------------------------------
+# Code below this line will not execute because of 'exit 0' above.
+
+# Stephane Chazelas suggests the following alternative:
+
+usage() {
+  echo "Usage: `basename $0` C-program-file" >&2
+  exit 1
+}
+
+WEIRD=`echo -n -e '\377'`   # or WEIRD=$'\377'
+[[ $# -eq 1 ]] || usage
+case `file "$1"` in
+  *"C program text"*) sed -e "s%/\*%${WEIRD}%g;s%\*/%${WEIRD}%g" "$1" \
+     | tr '\377\n' '\n\377' \
+     | sed -ne 'p;n' \
+     | tr -d '\n' | tr '\377' '\n';;
+  *) usage;;
+esac
+
+#  This is still fooled by things like:
+#  printf("/*");
+#  or
+#  /*  /* buggy embedded comment */
+#
+#  To handle all special cases (comments in strings, comments in string
+#+ where there is a \", \\" ...),
+#+ the only way is to write a C parser (using lex or yacc perhaps?).
+
+exit 0
+}
+
+}
+
+}
+
 
 ◊; emacs:
 ◊; Local Variables:
