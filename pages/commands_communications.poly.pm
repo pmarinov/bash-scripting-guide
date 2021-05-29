47ed1ec19183d784ef6956dca8203aacac340063
Peter Marinov <efravia@gmail.com>
2021-05-28 18:12:24 -0700

    chapter: Communications Commands (part 2)

--- pages/commands_communications.poly.pm
+++ pages/commands_communications.poly.pm
@@ -86,6 +86,262 @@ bash$ dig -x 81.9.6.2
 ;; MSG SIZE  rcvd: 91
 }
 
+◊anchored-example[#:anchor "dig_spam1"]{Finding out where to report a
+spammer}
+
+◊example{
+#!/bin/bash
+# spam-lookup.sh: Look up abuse contact to report a spammer.
+# Thanks, Michael Zick.
+
+# Check for command-line arg.
+ARGCOUNT=1
+E_WRONGARGS=85
+if [ $# -ne "$ARGCOUNT" ]
+then
+  echo "Usage: `basename $0` domain-name"
+  exit $E_WRONGARGS
+fi
+
+
+dig +short $1.contacts.abuse.net -c in -t txt
+# Also try:
+#     dig +nssearch $1
+#     Tries to find "authoritative name servers" and display SOA records.
+
+# The following also works:
+#     whois -h whois.abuse.net $1
+#           ^^ ^^^^^^^^^^^^^^^  Specify host.  
+#     Can even lookup multiple spammers with this, i.e."
+#     whois -h whois.abuse.net $spamdomain1 $spamdomain2 . . .
+
+
+#  Exercise:
+#  --------
+#  Expand the functionality of this script
+#+ so that it automatically e-mails a notification
+#+ to the responsible ISP's contact address(es).
+#  Hint: use the "mail" command.
+
+exit $?
+
+# spam-lookup.sh chinatietong.com
+#                A known spam domain.
+
+# "crnet_mgr@chinatietong.com"
+# "crnet_tec@chinatietong.com"
+# "postmaster@chinatietong.com"
+
+
+#  For a more elaborate version of this script,
+#+ see the SpamViz home page, http://www.spamviz.net/index.html.
+}
+
+
+◊anchored-example[#:anchor "dig_spam2"]{Analyzing a spam domain}
+
+◊example{
+#! /bin/bash
+# is-spammer.sh: Identifying spam domains
+
+# $Id: is-spammer, v 1.4 2004/09/01 19:37:52 mszick Exp $
+# Above line is RCS ID info.
+#
+#  This is a simplified version of the "is_spammer.bash
+#+ script in the Contributed Scripts appendix.
+
+# is-spammer <domain.name>
+
+# Uses an external program: 'dig'
+# Tested with version: 9.2.4rc5
+
+# Uses functions.
+# Uses IFS to parse strings by assignment into arrays.
+# And even does something useful: checks e-mail blacklists.
+
+# Use the domain.name(s) from the text body:
+# http://www.good_stuff.spammer.biz/just_ignore_everything_else
+#                       ^^^^^^^^^^^
+# Or the domain.name(s) from any e-mail address:
+# Really_Good_Offer@spammer.biz
+#
+# as the only argument to this script.
+#(PS: have your Inet connection running)
+#
+# So, to invoke this script in the above two instances:
+#       is-spammer.sh spammer.biz
+
+
+# Whitespace == :Space:Tab:Line Feed:Carriage Return:
+WSP_IFS=$'\x20'$'\x09'$'\x0A'$'\x0D'
+
+# No Whitespace == Line Feed:Carriage Return
+No_WSP=$'\x0A'$'\x0D'
+
+# Field separator for dotted decimal ip addresses
+ADR_IFS=${No_WSP}'.'
+
+# Get the dns text resource record.
+# get_txt <error_code> <list_query>
+get_txt() {
+
+    # Parse $1 by assignment at the dots.
+    local -a dns
+    IFS=$ADR_IFS
+    dns=( $1 )
+    IFS=$WSP_IFS
+    if [ "${dns[0]}" == '127' ]
+    then
+        # See if there is a reason.
+        echo $(dig +short $2 -t txt)
+    fi
+}
+
+# Get the dns address resource record.
+# chk_adr <rev_dns> <list_server>
+chk_adr() {
+    local reply
+    local server
+    local reason
+
+    server=${1}${2}
+    reply=$( dig +short ${server} )
+
+    # If reply might be an error code . . .
+    if [ ${#reply} -gt 6 ]
+    then
+        reason=$(get_txt ${reply} ${server} )
+        reason=${reason:-${reply}}
+    fi
+    echo ${reason:-' not blacklisted.'}
+}
+
+# Need to get the IP address from the name.
+echo 'Get address of: '$1
+ip_adr=$(dig +short $1)
+dns_reply=${ip_adr:-' no answer '}
+echo ' Found address: '${dns_reply}
+
+# A valid reply is at least 4 digits plus 3 dots.
+if [ ${#ip_adr} -gt 6 ]
+then
+    echo
+    declare query
+
+    # Parse by assignment at the dots.
+    declare -a dns
+    IFS=$ADR_IFS
+    dns=( ${ip_adr} )
+    IFS=$WSP_IFS
+
+    # Reorder octets into dns query order.
+    rev_dns="${dns[3]}"'.'"${dns[2]}"'.'"${dns[1]}"'.'"${dns[0]}"'.'
+
+# See: http://www.spamhaus.org (Conservative, well maintained)
+    echo -n 'spamhaus.org says: '
+    echo $(chk_adr ${rev_dns} 'sbl-xbl.spamhaus.org')
+
+# See: http://ordb.org (Open mail relays)
+    echo -n '   ordb.org  says: '
+    echo $(chk_adr ${rev_dns} 'relays.ordb.org')
+
+# See: http://www.spamcop.net/ (You can report spammers here)
+    echo -n ' spamcop.net says: '
+    echo $(chk_adr ${rev_dns} 'bl.spamcop.net')
+
+# # # other blacklist operations # # #
+
+# See: http://cbl.abuseat.org.
+    echo -n ' abuseat.org says: '
+    echo $(chk_adr ${rev_dns} 'cbl.abuseat.org')
+
+# See: http://dsbl.org/usage (Various mail relays)
+    echo
+    echo 'Distributed Server Listings'
+    echo -n '       list.dsbl.org says: '
+    echo $(chk_adr ${rev_dns} 'list.dsbl.org')
+
+    echo -n '   multihop.dsbl.org says: '
+    echo $(chk_adr ${rev_dns} 'multihop.dsbl.org')
+
+    echo -n 'unconfirmed.dsbl.org says: '
+    echo $(chk_adr ${rev_dns} 'unconfirmed.dsbl.org')
+
+else
+    echo
+    echo 'Could not use that address.'
+fi
+
+exit 0
+
+# Exercises:
+# --------
+
+# 1) Check arguments to script,
+#    and exit with appropriate error message if necessary.
+
+# 2) Check if on-line at invocation of script,
+#    and exit with appropriate error message if necessary.
+
+# 3) Substitute generic variables for "hard-coded" BHL domains.
+
+# 4) Set a time-out for the script using the "+time=" option
+     to the 'dig' command.
+}
+
+For a much more elaborate version of the above script, see Example
+TODO A-28.
+}
+
+◊definition-entry[#:name "traceroute"]{
+Trace the route taken by packets sent to a remote host. This command
+works within a LAN, WAN, or over the Internet. The remote host may be
+specified by an IP address. The output of this command may be filtered
+by ◊command{grep} or ◊command{sed} in a pipe.
+
+◊example{
+bash$ traceroute 81.9.6.2
+traceroute to 81.9.6.2 (81.9.6.2), 30 hops max, 38 byte packets
+1  tc43.xjbnnbrb.com (136.30.178.8)  191.303 ms  179.400 ms  179.767 ms
+2  or0.xjbnnbrb.com (136.30.178.1)  179.536 ms  179.534 ms  169.685 ms
+3  192.168.11.101 (192.168.11.101)  189.471 ms  189.556 ms *
+...
+}
+
+}
+
+◊definition-entry[#:name "ping"]{
+
+Broadcast an ICMP ECHO_REQUEST packet to another machine, either on a
+local or remote network.
+
+◊example{
+bash$ ping localhost
+PING localhost.localdomain (127.0.0.1) from 127.0.0.1 : 56(84) bytes of data.
+64 bytes from localhost.localdomain (127.0.0.1): icmp_seq=0 ttl=255 time=709 usec
+64 bytes from localhost.localdomain (127.0.0.1): icmp_seq=1 ttl=255 time=286 usec
+
+--- localhost.localdomain ping statistics ---
+2 packets transmitted, 2 packets received, 0% packet loss
+round-trip min/avg/max/mdev = 0.286/0.497/0.709/0.212 ms
+}
+
+A successful ◊command{ping} returns an exit status of 0. This can be
+tested for in a script.
+
+◊example{
+HNAME=news-15.net  # Notorious spammer.
+# HNAME=$HOST     # Debug: test for localhost.
+count=2  # Send only two pings.
+
+if [[ `ping -c $count "$HNAME"` ]]
+then
+  echo ""$HNAME" still up and broadcasting spam your way."
+else
+  echo ""$HNAME" seems to be down. Pity."
+fi
+}
+
 }
 
 
