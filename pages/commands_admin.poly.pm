cd69fa1dc576f247fd8b7f80540f7066b133a3cc
Peter Marinov <efravia@gmail.com>
2021-08-14 13:01:57 -0700

    chapter: Admin commands (part 3)

--- pages/commands_admin.poly.pm
+++ pages/commands_admin.poly.pm
@@ -197,6 +197,290 @@ process. As we have just seen, sometimes these are not the same.
 }
 
 ◊definition-entry[#:name "su"]{
+Runs a program or script as a substitute user. ◊command{su rjones}
+starts a shell as user rjones. A naked ◊command{su} defaults to
+root. See TODO Example A-14.
+
+}
+
+◊definition-entry[#:name "sudo"]{
+Runs a command as root (or another user). This may be used in a
+script, thus permitting a regular user to run the script.
+
+◊example{
+#!/bin/bash
+
+# Some commands.
+sudo cp /root/secretfile /home/bozo/secret
+# Some more commands.
+}
+
+The file ◊fname{/etc/sudoers} holds the names of users permitted to
+invoke sudo.
+
+}
+
+◊definition-entry[#:name "passwd"]{
+Sets, changes, or manages a user's password.
+
+The ◊command{passwd} command can be used in a script, but probably
+should not be.
+
+◊example{
+#!/bin/bash
+#  setnew-password.sh: For demonstration purposes only.
+#                      Not a good idea to actually run this script.
+#  This script must be run as root.
+
+ROOT_UID=0         # Root has $UID 0.
+E_WRONG_USER=65    # Not root?
+
+E_NOSUCHUSER=70
+SUCCESS=0
+
+
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo; echo "Only root can run this script."; echo
+  exit $E_WRONG_USER
+else
+  echo
+  echo "You should know better than to run this script, root."
+  echo "Even root users get the blues... "
+  echo
+fi
+
+
+username=bozo
+NEWPASSWORD=security_violation
+
+# Check if bozo lives here.
+grep -q "$username" /etc/passwd
+if [ $? -ne $SUCCESS ]
+then
+  echo "User $username does not exist."
+  echo "No password changed."
+  exit $E_NOSUCHUSER
+fi
+
+echo "$NEWPASSWORD" | passwd --stdin "$username"
+#  The '--stdin' option to 'passwd' permits
+#+ getting a new password from stdin (or a pipe).
+
+echo; echo "User $username's password changed!"
+
+# Using the 'passwd' command in a script is dangerous.
+
+exit 0
+}
+
+The passwd command's ◊code{-l}, ◊code{-u}, and ◊code{-d} options
+permit locking, unlocking, and deleting a user's password. Only root
+may use these options.
+
+}
+
+◊definition-entry[#:name "ac"]{
+Show users' logged in time, as read from ◊fname{/var/log/wtmp}. This
+is one of the GNU accounting utilities.
+
+◊example{
+bash$ ac
+total       68.08
+}
+
+}
+
+◊definition-entry[#:name "last"]{
+List last logged in users, as read from ◊fname{/var/log/wtmp}. This
+command can also show remote logins.
+
+For example, to show the last few times the system rebooted:
+
+◊example{
+bash$ last reboot
+reboot   system boot  2.6.9-1.667      Fri Feb  4 18:18          (00:02)
+reboot   system boot  2.6.9-1.667      Fri Feb  4 15:20          (01:27)
+reboot   system boot  2.6.9-1.667      Fri Feb  4 12:56          (00:49)
+reboot   system boot  2.6.9-1.667      Thu Feb  3 21:08          (02:17)
+. . .
+
+wtmp begins Tue Feb  1 12:50:09 2005
+}
+
+}
+
+◊definition-entry[#:name "newgrp"]{
+Change user's group ID without logging out. This permits access to the
+new group's files. Since users may be members of multiple groups
+simultaneously, this command finds only limited use.
+
+Note: The newgrp command could prove helpful in setting the default
+group permissions for files a user writes. However, the
+◊command{chgrp} command might be more convenient for this purpose.
+
+}
+
+}
+
+◊section{Terminals}
+
+◊definition-block[#:type "code"]{
+
+◊definition-entry[#:name "tty"]{
+Echoes the name (filename) of the current user's terminal. Note that
+each separate xterm window counts as a different terminal.
+
+◊example{
+bash$ tty
+/dev/pts/1
+}
+
+}
+
+◊definition-entry[#:name "stty"]{
+
+Shows and/or changes terminal settings. This complex command, used in
+a script, can control terminal behavior and the way output
+displays. See the info page, and study it carefully
+
+◊example{
+bash$ info stty
+}
+
+◊anchored-example[#:anchor "stty_erase1"]{Setting an erase character}
+
+◊example{
+#!/bin/bash
+# erase.sh: Using "stty" to set an erase character when reading input.
+
+echo -n "What is your name? "
+read name                      #  Try to backspace
+                               #+ to erase characters of input.
+                               #  Problems?
+echo "Your name is $name."
+
+stty erase '#'                 #  Set "hashmark" (#) as erase character.
+echo -n "What is your name? "
+read name                      #  Use # to erase last character typed.
+echo "Your name is $name."
+
+exit 0
+
+# Even after the script exits, the new key value remains set.
+# Exercise: How would you reset the erase character to the default value?
+}
+
+◊anchored-example[#:anchor "stty_noecho1"]{secret password: Turning
+off terminal echoing}
+
+◊example{
+#!/bin/bash
+# secret-pw.sh: secret password
+
+echo
+echo -n "Enter password "
+read passwd
+echo "password is $passwd"
+echo -n "If someone had been looking over your shoulder, "
+echo "your password would have been compromised."
+
+echo && echo  # Two line-feeds in an "and list."
+
+
+stty -echo    # Turns off screen echo.
+#   May also be done with
+#   read -sp passwd
+#   A big Thank You to Leigh James for pointing this out.
+
+echo -n "Enter password again "
+read passwd
+echo
+echo "password is $passwd"
+echo
+
+stty echo     # Restores screen echo.
+
+exit 0
+
+# Do an 'info stty' for more on this useful-but-tricky command.
+}
+
+◊anchored-example[#:anchor "stty_kbd"]{Keypress detection}
+
+A creative use of ◊command{stty} is detecting a user keypress (without
+hitting ENTER).
+
+◊example{
+#!/bin/bash
+# keypress.sh: Detect a user keypress ("hot keys").
+
+echo
+
+old_tty_settings=$(stty -g)   # Save old settings (why?).
+stty -icanon
+Keypress=$(head -c1)          # or $(dd bs=1 count=1 2> /dev/null)
+                              # on non-GNU systems
+
+echo
+echo "Key pressed was \""$Keypress"\"."
+echo
+
+stty "$old_tty_settings"      # Restore old settings.
+
+# Thanks, Stephane Chazelas.
+
+exit 0
+}
+
+Also see TODO Example 9-3 and Example A-43.
+
+Normally, a terminal works in the canonical mode. When a user hits a
+key, the resulting character does not immediately go to the program
+actually running in this terminal. A buffer local to the terminal
+stores keystrokes. When the user hits the ENTER key, this sends all
+the stored keystrokes to the program running. There is even a basic
+line editor inside the terminal.
+
+◊example{
+bash$ stty -a
+speed 9600 baud; rows 36; columns 96; line = 0;
+intr = ^C; quit = ^\; erase = ^H; kill = ^U; eof = ^D; eol = <undef>; eol2 = <undef>;
+start = ^Q; stop = ^S; susp = ^Z; rprnt = ^R; werase = ^W; lnext = ^V; flush = ^O;
+...
+isig icanon iexten echo echoe echok -echonl -noflsh -xcase -tostop -echoprt
+}
+
+Using canonical mode, it is possible to redefine the special keys for
+the local terminal line editor.
+
+◊example{
+bash$ cat > filexxx
+wha<ctl-W>I<ctl-H>foo bar<ctl-U>hello world<ENTER>
+<ctl-D>
+bash$ cat filexxx
+hello world
+bash$ wc -c < filexxx
+12
+}
+
+
+The process controlling the terminal receives only 12 characters (11
+alphabetic ones, plus a newline), although the user hit 26 keys.
+
+In non-canonical ("raw") mode, every key hit (including special
+editing keys such as ◊kbd{ctl-H}) sends a character immediately to the
+controlling process.
+
+The Bash prompt disables both ◊code{icanon} and ◊code{echo}, since it
+replaces the basic terminal line editor with its own more elaborate
+one. For example, when you hit ◊kbd{ctl-A} at the Bash prompt, there's
+no ^A echoed by the terminal, but Bash gets a \1 character, interprets
+it, and moves the cursor to the begining of the line.
+
+}
+
+◊definition-entry[#:name "setterm"]{
 }
 
 }
