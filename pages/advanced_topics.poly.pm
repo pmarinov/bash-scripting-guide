5f22038678f302bed63493747601bdb67672dc3c
Peter Marinov <efravia@gmail.com>
2021-09-24 22:13:12 -0700

    chapter: Introduction to Regular Expressions (part 1)

---
+++ pages/advanced_topics.poly.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊; This is free software released into the public domain
+◊; See LICENSE.txt
+
+◊page-init{}
+◊define-meta[page-title]{Advanced Topics}
+◊define-meta[page-description]{Advanced Topics}
+
+At this point, we are ready to delve into certain of the difficult and
+unusual aspects of scripting. Along the way, we will attempt to "push
+the envelope" in various ways and examine boundary conditions (what
+happens when we move into uncharted territory?).
+
+◊; List pages under commands.poly.pm
+◊(node-menu "pages/advanced_topics.poly.pm")
+
---
+++ pages/regexp_intro.poly.pm
@@ -0,0 +1,7 @@
+#lang pollen
+
+◊page-init{}
+◊define-meta[page-title]{Introduction to Regular Expressions}
+◊define-meta[page-description]{A Brief Introduction to Regular Expressions}
+
+◊section{What is a regular expression}
