a5070a8610cba56b74f9f97f9fdb51fb4123458e
Peter Marinov <efravia@gmail.com>
2021-12-06 20:46:06 -0800

    section: Discplaimer

--- pages/legal.poly.pm
+++ pages/legal.poly.pm
@@ -68,3 +68,19 @@ Revision 6.5 05 Apr 2012 Revised by: mc
 
 See AUTHORS in the source folder
 
+◊section["Disclaimer"]
+
+(This is a variant of the standard LDP disclaimer.)
+
+No liability for the contents of this document can be accepted. Use
+the concepts, examples and information at your own risk. There may be
+errors, omissions, and inaccuracies that could cause you to lose data,
+harm your system, or induce involuntary electrocution, so proceed with
+appropriate caution. The author takes no responsibility for any
+damages, incidental or otherwise.
+
+As it happens, it is highly unlikely that either you or your system
+will suffer ill effects, aside from uncontrollable hiccups. In fact,
+the raison d'etre of this book is to enable its readers to analyze
+shell scripts and determine whether they have unanticipated
+consequences.
