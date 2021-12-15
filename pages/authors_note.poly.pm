8a1ea6d485a4987952c1b6810d97c52db1e46937
Peter Marinov <efravia@gmail.com>
2021-12-14 21:29:09 -0800

    chapter: Author's Note

---
+++ pages/authors_note.poly.pm
@@ -0,0 +1,69 @@
+#lang pollen
+
+◊page-init{}
+◊define-meta[page-title]{Author's Note}
+◊define-meta[page-description]{Author's Note}
+
+◊quotation[#:author "-"]{doce ut discas
+
+(Teach, that you yourself may learn.)}
+
+◊section{Note}
+
+How did I come to write a scripting book? It's a strange tale. It
+seems that a few years back I needed to learn shell scripting -- and
+what better way to do that than to read a good book on the subject? I
+was looking to buy a tutorial and reference covering all aspects of
+the subject. I was looking for a book that would take difficult
+concepts, turn them inside out, and explain them in excruciating
+detail, with well-commented examples. [1] In fact, I was looking for
+this very book, or something very much like it. Unfortunately, it
+didn't exist, and if I wanted it, I'd have to write it. And so, here
+we are, folks.
+
+That reminds me of the apocryphal story about a mad professor. Crazy
+as a loon, the fellow was. At the sight of a book, any book -- at the
+library, at a bookstore, anywhere -- he would become totally obsessed
+with the idea that he could have written it, should have written it --
+and done a better job of it to boot. He would thereupon rush home and
+proceed to do just that, write a book with the very same title. When
+he died some years later, he allegedly had several thousand books to
+his credit, probably putting even Asimov to shame. The books might not
+have been any good, who knows, but does that really matter? Here's a
+fellow who lived his dream, even if he was obsessed by it, driven by
+it . . . and somehow I can't help admiring the old coot.
+
+◊section{Hardare used}
+
+A used IBM Thinkpad, model 760XL laptop (P166, 104 meg RAM) running
+Red Hat 7.1/7.3. Sure, it's slow and has a funky keyboard, but it
+beats the heck out of a No. 2 pencil and a Big Chief tablet.
+
+Update: upgraded to a 770Z Thinkpad (P2-366, 192 meg RAM) running
+FC3. Anyone feel like donating a later-model laptop to a starving
+writer?
+
+Update: upgraded to a T61 Thinkpad running Mandriva 2011. No longer
+starving, but not too proud to accept donations.
+
+◊section{Software used}
+
+◊list-block[#:type "bullet"]{
+
+◊list-entry{Bram Moolenaar's powerful SGML-aware vim text editor.}
+
+◊list-entry{OpenJade, a DSSSL rendering engine for converting SGML
+documents into other formats.}
+
+◊list-entry{Norman Walsh's DSSSL stylesheets.}
+
+◊list-entry{DocBook, The Definitive Guide, by Norman Walsh and Leonard
+Muellner (O'Reilly, ISBN 1-56592-580-7). This is still the standard
+reference for anyone attempting to write a document in Docbook SGML
+format.}
+
+}
+
+◊section{Credits}
+
+See AUTHORS in the source folder
