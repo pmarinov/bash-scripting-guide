1015dbd2d70e2f68ed84b3ed8bccb247abbce0dc
Peter Marinov <efravia@gmail.com>
2021-12-20 21:53:29 -0800

    chapter: Bibliography (part 1)

---
+++ pages/bibliography.poly.pm
@@ -0,0 +1,101 @@
+#lang pollen
+
+◊page-init{}
+◊define-meta[page-title]{Bibliography}
+◊define-meta[page-description]{Bibliography}
+
+◊quotation[#:author "Henry Spencer"]{Those who do not understand UNIX
+are condemned to reinvent it, poorly.}
+
+◊list-block[#:type "bullet"]{
+
+◊list-entry{Edited by Peter Denning, "Computers Under Attack:
+Intruders, Worms, and Viruses", ACM Press, 1990, 0-201-53067-8.
+
+This compendium contains a couple of articles on shell script viruses.
+
+}
+
+◊list-entry{Ken Burtch, "Linux Shell Scripting with Bash", 1st
+edition, Sams Publishing (Pearson), 2004, 0672326426.
+
+Covers much of the same material as the ABS Guide, though in a
+different style.
+
+}
+
+◊list-entry{Daniel Goldman, "Definitive Guide to Sed", 1st edition,
+2013.
+
+This ebook is an excellent introduction to sed. Rather than being a
+conversion from a printed volume, it was specifically designed and
+formatted for viewing on an ebook reader. Well-written, informative,
+and useful as a reference as well as a tutorial. Highly recommended.
+
+}
+
+◊list-entry{Dale Dougherty and Arnold Robbins, "Sed and Awk, 2nd
+edition", O'Reilly and Associates, 1997, 1-156592-225-5.
+
+Unfolding the full power of shell scripting requires at least a
+passing familiarity with sed and awk. This is the classic tutorial. It
+includes an excellent introduction to Regular
+Expressions. Recommended.
+
+}
+
+◊list-entry{Jeffrey Friedl, "Mastering Regular Expressions", O'Reilly
+and Associates, 2002, 0-596-00289-0.
+
+Still the best all-around reference on Regular Expressions.
+
+}
+
+◊list-entry{Aeleen Frisch, "Essential System Administration", 3rd
+edition, O'Reilly and Associates, 2002, 0-596-00343-9.
+
+This excellent manual provides a decent introduction to shell
+scripting from a sys admin point of view. It includes comprehensive
+explanations of the startup and initialization scripts in a UNIX
+system.
+
+}
+
+◊list-entry{Stephen Kochan and Patrick Wood, "Unix Shell Programming",
+Hayden, 1990, 067248448X.
+
+Still considered a standard reference, though somewhat dated, and a
+bit "wooden" stylistically speaking. ◊footnote{It was hard to resist
+the obvious pun. No slight intended, since the book is a pretty decent
+introduction to the basic concepts of shell scripting.} In fact, this
+book was the ABS Guide author's first exposure to UNIX shell
+scripting, lo these many years ago.
+
+}
+
+◊list-entry{Neil Matthew and Richard Stones, "Beginning Linux
+Programming", Wrox Press, 1996, 1874416680.
+
+Surprisingly good in-depth coverage of various programming languages
+available for Linux, including a fairly strong chapter on shell
+scripting.
+
+}
+
+◊list-entry{Herbert Mayer, "Advanced C Programming on the IBM PC",
+Windcrest Books, 1989, 0830693637.
+
+Excellent coverage of algorithms and general programming
+practices. Highly recommended, but unfortunately out of print.
+
+}
+
+◊list-entry{David Medinets, "Unix Shell Programming Tools",
+McGraw-Hill, 1999, 0070397333.
+
+Pretty good treatment of shell scripting, with examples, and a short
+intro to Tcl and Perl.
+
+}
+
+}
