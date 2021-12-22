cf5449be997f68a7132ddbe7e2e25192cb213cfb
Peter Marinov <efravia@gmail.com>
2021-12-21 22:13:40 -0800

    chapter: Bibliography (part 2)

--- pages/bibliography.poly.pm
+++ pages/bibliography.poly.pm
@@ -7,6 +7,8 @@
 ◊quotation[#:author "Henry Spencer"]{Those who do not understand UNIX
 are condemned to reinvent it, poorly.}
 
+◊section{Books}
+
 ◊list-block[#:type "bullet"]{
 
 ◊list-entry{Edited by Peter Denning, "Computers Under Attack:
@@ -95,6 +97,175 @@ McGraw-Hill, 1999, 0070397333.
 
 Pretty good treatment of shell scripting, with examples, and a short
 intro to Tcl and Perl.
+
+}
+
+◊list-entry{Cameron Newham and Bill Rosenblatt, "Learning the Bash
+Shell", 2nd edition, O'Reilly and Associates, 1998, 1-56592-347-2.
+
+This is a valiant effort at a decent shell primer, but sadly deficient
+in its coverage of writing scripts and lacking sufficient examples.
+
+}
+
+◊list-entry{Anatole Olczak, "Bourne Shell Quick Reference Guide", ASP,
+Inc., 1991, 093573922X.
+
+A very handy pocket reference, despite lacking coverage of
+Bash-specific features.
+
+}
+
+◊list-entry{Jerry Peek, Tim O'Reilly, and Mike Loukides, "Unix Power
+Tools", 3rd edition, O'Reilly and Associates, Random House, 2002,
+0-596-00330-7.
+
+Contains a couple of sections of very informative in-depth articles on
+shell programming, but falls short of being a self-teaching manual. It
+reproduces much of the Regular Expressions tutorial from the Dougherty
+and Robbins book, above. The comprehensive coverage of UNIX commands
+makes this book worthy of a place on your bookshelf.
+
+}
+
+◊list-entry{Clifford Pickover, Computers, Pattern, Chaos, and Beauty,
+St. Martin's Press, 1990, 0-312-04123-3.
+
+A treasure trove of ideas and recipes for computer-based exploration
+of mathematical oddities.
+
+}
+
+◊list-entry{George Polya, "How To Solve It", Princeton University Press,
+1973, 0-691-02356-5.
+
+The classic tutorial on problem-solving methods (algorithms), with
+special emphasis on how to teach them.
+
+}
+
+◊list-entry{Chet Ramey and Brian Fox, "The GNU Bash Reference Manual",
+Network Theory Ltd, 2003, 0-9541617-7-7.
+
+This manual is the definitive reference for GNU Bash. The authors of
+this manual, Chet Ramey and Brian Fox, are the original developers of
+GNU Bash. For each copy sold, the publisher donates $1 to the Free
+Software Foundation.
+
+}
+
+◊list-entry{Arnold Robbins, "Bash Reference Card", SSC, 1998,
+1-58731-010-5.
+
+Excellent Bash pocket reference (don't leave home without it,
+especially if you're a sysadmin). A bargain at $4.95, but
+unfortunately no longer available for free download.
+
+}
+
+◊list-entry{Arnold Robbins, "Effective Awk Programming", Free Software
+Foundation / O'Reilly and Associates, 2000, 1-882114-26-4.
+
+The absolute best awk tutorial and reference. The free electronic
+version of this book is part of the awk documentation, and printed
+copies of the latest version are available from O'Reilly and
+Associates.
+
+This book served as an inspiration for the author of the ABS Guide.
+
+}
+
+◊list-entry{Bill Rosenblatt, "Learning the Korn Shell", O'Reilly and
+Associates, 1993, 1-56592-054-6.
+
+This well-written book contains some excellent pointers on shell
+scripting in general.
+
+}
+
+◊list-entry{Paul Sheer, LINUX: Rute User's Tutorial and Exposition,
+1st edition, , 2002, 0-13-033351-4.
+
+Very detailed and readable introduction to Linux system
+administration.
+
+The book is available in print, or on-line.
+
+}
+
+◊list-entry{Ellen Siever and the staff of O'Reilly and Associates,
+Linux in a Nutshell, 2nd edition, O'Reilly and Associates, 1999,
+1-56592-585-8.
+
+The all-around best Linux command reference. It even has a Bash
+section.
+
+}
+
+◊list-entry{Dave Taylor, "Wicked Cool Shell Scripts: 101 Scripts for
+Linux, Mac OS X, and Unix Systems", 1st edition, No Starch Press,
+2004, 1-59327-012-7.
+
+Pretty much what the title promises . . .
+
+}
+
+◊list-entry{"The UNIX CD Bookshelf", 3rd edition, O'Reilly and
+Associates, 2003, 0-596-00392-7.
+
+An array of seven UNIX books on CD ROM, including UNIX Power Tools,
+Sed and Awk, and Learning the Korn Shell. A complete set of all the
+UNIX references and tutorials you would ever need at about $130. Buy
+this one, even if it means going into debt and not paying the rent.
+
+Update: Seems to have somehow fallen out of print. Ah, well. You can
+still buy the dead-tree editions of these books.
+
+}
+
+◊list-entry{The O'Reilly books on Perl. (Actually, any O'Reilly
+books.)
+
+}
+
+}
+
+◊section{Other resources}
+
+◊list-block[#:type "bullet"]{
+
+◊list-entry{Fioretti, Marco, "Scripting for X Productivity," Linux
+Journal, Issue 113, September, 2003, pp. 86-9.
+
+}
+
+◊list-entry{Chet Ramey's Bash - The GNU Shell, a two-part series
+published in issues 3 and 4 of the Linux Journal, July-August 1994.
+
+}
+
+◊list-entry{Mike G's "Bash-Programming-Intro HOWTO".
+
+See: ◊url[#:link
+"https://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO.html"]{}
+
+}
+
+◊list-entry{Chet Ramey's "Bash FAQ".
+
+See: ◊url[#:link "http://tiswww.case.edu/php/chet/bash/FAQ"]{}
+
+}
+
+◊list-entry{Greg's Wiki, many Bash related pages
+
+See: ◊url[#:link "http://mywiki.wooledge.org/"]{}
+
+}
+
+◊list-entry{Shell scripts at SHELLdorado
+
+See: ◊url[#:link "http://shelldorado.com/"]{}
 
 }
 
