fa80b5e0de0d034e624460b513642d8904535bc3
Peter Marinov <efravia@gmail.com>
2022-01-04 21:09:54 -0800

    chapter: Reference Cards (part 3)

--- pages/reference_cards.poly.pm
+++ pages/reference_cards.poly.pm
@@ -148,5 +148,75 @@ Within double parentheses ◊code{(( ... ))}:
 Note: Some of the above behavior of operators has changed from earlier
 versions of Bash.
 
+◊section{String Operations}
+
+◊example{
++----------------------------------+---------------------------------------------+
+| Expression                       | Meaning                                     |
+|----------------------------------+---------------------------------------------|
+| ${#string}                       | Length of $string                           |
+|                                  |                                             |
+| ${string:position}               | Extract from $string at $position           |
+|                                  |                                             |
+| ${string:position:length}        | Extract $length characters from $string     |
+|                                  | at $position [starting from 0]              |
+|                                  |                                             |
+| ${string#substring}              | Strip shortest match of $substring from     |
+|                                  | front of $string                            |
+|                                  |                                             |
+| ${string##substring}             | Strip longest match of $substring from      |
+|                                  | front of $string                            |
+|                                  |                                             |
+| ${string%substring}              | Strip shortest match of $substring from     |
+|                                  | back of $string                             |
+|                                  |                                             |
+| ${string%%substring}             | Strip longest match of $substring           |
+|                                  | from back of $string                        |
+|                                  |                                             |
+| ${string/substring/replacement}  | Replace first match of $substring           |
+|                                  | with $replacement                           |
+|                                  |                                             |
+| ${string//substring/replacement} | Replace all matches of $substring           |
+|                                  | with $replacement                           |
+|                                  |                                             |
+| ${string/#substring/replacement} | If $substring matches front end of $string, |
+|                                  | substitute $replacement for $substring      |
+|                                  |                                             |
+| ${string/%substring/replacement} | If $substring matches back end of $string,  |
+|                                  | substitute $replacement for $substring      |
++----------------------------------+---------------------------------------------+
+}
+
+◊section{String Operations with 'expr'}
+
+◊example{
++-----------------------------------------+-----------------------------------------------------+
+| Expression                              | Meaning                                             |
+|-----------------------------------------+-----------------------------------------------------|
+| expr match "$string" '$substring'       | Length of matching $substring*                      |
+|                                         | at beginning of $string                             |
+|                                         |                                                     |
+| expr "$string" : '$substring'           | Length of matching $substring*                      |
+|                                         | at beginning of $string                             |
+|                                         |                                                     |
+| expr index "$string" $substring         | Numerical position in $string of first character in |
+|                                         | $substring* that matches [0 if no match, first      |
+|                                         | character counts as position 1]                     |
+|                                         |                                                     |
+| expr substr $string $position $length   | Extract $length characters from $string starting    |
+|                                         | at $position [0 if no match, first character counts |
+|                                         | as position 1]                                      |
+|                                         |                                                     |
+| expr match "$string" '\($substring\)'   | Extract $substring*, searching from beginning       |
+|                                         |                                                     |
+| expr "$string" : '\($substring\)'       | Extract $substring* , searching from beginning      |
+|                                         |                                                     |
+| expr match "$string" '.*\($substring\)' | Extract $substring*, searching from end             |
+|                                         |                                                     |
+| expr "$string" : '.*\($substring\)'     | Extract $substring*, searching from end             |
++-----------------------------------------+-----------------------------------------------------+
+}
+
+* Where ◊code{$substring} is a Regular Expression.
 
 
